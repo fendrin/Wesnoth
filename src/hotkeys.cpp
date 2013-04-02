fendrin/Wesnoth

commit 3d5e55e87a980fda73709df22fb4c55ca7cae8e9

Handle unrecognized hotkey commands more gracefull.
Former-commit-id: bb44cea2c3003a10a8c10ebed98d73c9979ab758
Former-commit-id: 060925a87b12e7274c9bd72bd9c7622bfd8b004c
Former-commit-id: c473328230364671170005e93b6599b59e8829d9
Former-commit-id: 889f52e044d4365ab2eb25ccba54a04991ad90b2 [formerly c4c55ddba10133c624fe53478d1ca0d78e4fe84e]
Former-commit-id: 956e9587f5cb55b815b57c916e1a8eb35b4ffc30

src/hotkeys.cpp
@@ -231,7 +231,7 @@ const hotkey_command hotkey_list_[] = {
 	{ hotkey::GLOBAL__HELPTIP, "global__helptip", N_("Show Helptip"), false, hotkey::SCOPE_GENERAL },
 
 	//This list item must stay at the end since it is used as terminator for iterating.
-	{ hotkey::HOTKEY_NULL, "null", NULL, true, hotkey::SCOPE_GENERAL }
+	{ hotkey::HOTKEY_NULL, "null", N_("Unrecognized Command"), true, hotkey::SCOPE_GENERAL }
 };
 
 const hotkey_command* get_hotkey_commands() {
@@ -515,7 +515,7 @@ manager::manager()
 
 void manager::init()
 {
-	for (size_t i = 0; hotkey_list_[i].id != hotkey::HOTKEY_NULL; ++i) {
+	for (size_t i = 0; hotkey_list_[i-1].id != hotkey::HOTKEY_NULL; ++i) {
 		command_map_[hotkey_list_[i].command] = i;
 	}
 }
@@ -590,7 +590,11 @@ void save_hotkeys(config& cfg)
 }
 
 const std::string get_description(const std::string& command) {
-	return t_string(hotkey_list_[command_map_[command]].description,
+	std::string tmp(command);
+	if (command_map_.find(tmp) == command_map_.end()) {
+		tmp = "null";
+	}
+	return t_string(hotkey_list_[command_map_[tmp]].description,
 			PACKAGE "-lib");
 }
 

commit c9ecb0c23d5961980aa223b45f54c6e4cd963736

Fix accessing an negative array index.
The problem was introduced in 3d5e55e87a980fda73709df22fb4c55ca7cae8e9 [formerly 956e9587f5cb55b815b57c916e1a8eb35b4ffc30] [formerly 889f52e044d4365ab2eb25ccba54a04991ad90b2 [formerly c4c55ddba10133c624fe53478d1ca0d78e4fe84e]] [formerly c473328230364671170005e93b6599b59e8829d9] [formerly 060925a87b12e7274c9bd72bd9c7622bfd8b004c] [formerly bb44cea2c3003a10a8c10ebed98d73c9979ab758].
Fendrin please review the fix.


Former-commit-id: 97f9a5ea2ce998cd227ee8fd7808ecc61eb26c1a
Former-commit-id: d2fe696e0abc4a78ba44cdc882e3c31ba9cc0239
Former-commit-id: acfc174da3ac8030761793b572d912f175633d66
Former-commit-id: 0f4b2b530beaf320c3891613af5f8de14ce8301c [formerly 226bc3e44245b143d651f97f3ead5f0a14f18e27]
Former-commit-id: 0bda4b61b61af9722567be5dacab5e5144d1d4c3

src/hotkeys.cpp
@@ -515,7 +515,7 @@ manager::manager()
 
 void manager::init()
 {
-	for (size_t i = 0; hotkey_list_[i-1].id != hotkey::HOTKEY_NULL; ++i) {
+	for (size_t i = 0; hotkey_list_[i].id != hotkey::HOTKEY_NULL; ++i) {
 		command_map_[hotkey_list_[i].command] = i;
 	}
 }