fendrin/Wesnoth

commit a14bde5e90941b3358ce66b9a3b9516e0ec17e21

fixed parsing of [target] tags...
...and tweaked SoF 8 to [target] ONLY side 1. Fixes bug #14178
(requires loading from start-of-scenario save)


Former-commit-id: ce11fc4cc4ed20e5155bc0fbc8b5cb7f4c1d435e
Former-commit-id: bb162078a5a0008701061715c260be85f5af1ac3

src/ai/contexts.cpp
@@ -1025,4 +1025,27 @@ std::map<std::pair<map_location,const unit_type *>,
 }
 
 
+bool readonly_context_impl::is_active(const std::string &time_of_day, const std::string &turns) const
+{
+		if(time_of_day.empty() == false) {
+			const std::vector<std::string>& times = utils::split(time_of_day);
+			if(std::count(times.begin(),times.end(),this->get_info().tod_manager_.get_time_of_day().name) == 0) {
+				return false;
+			}
+		}
+
+		if(turns.empty() == false) {
+			int turn = this->get_info().tod_manager_.turn();
+			const std::vector<std::string>& turns_list = utils::split(turns);
+			for(std::vector<std::string>::const_iterator j = turns_list.begin(); j != turns_list.end() ; j++ ) {
+				const std::pair<int,int> range = utils::parse_range(*j);
+				if(turn >= range.first && turn <= range.second) {
+				      return true;
+				}
+			}
+			return false;
+		}
+		return true;
+}
+
 } //of namespace ai

src/ai/default/contexts.cpp
@@ -326,6 +326,10 @@ std::vector<target> default_ai_context_impl::find_targets(unit_map::const_iterat
 
 		for(std::vector<goal_ptr>::iterator j = goals.begin();
 		    j != goals.end(); ++j) {
+
+			if (!(*j)->active()) {
+				continue;
+			}
 			if ((*j)->matches_unit(u)) {
 				LOG_AI << "found explicit target... " << u->first << " with value: " << (*j)->value() << "\n";
 				targets.push_back(target(u->first,(*j)->value(),target::EXPLICIT));

data/campaigns/Sceptre_of_Fire/scenarios/8_The_Dragon.cfg
@@ -44,7 +44,8 @@
             turns=1-12
             aggression=0.8
             caution=0.2
-            village_value=10
+            leader_value=0
+            village_value=0
             recruitment_pattern=scout,fighter,fighter,mixed fighter
             recruitment_ignore_bad_movement=yes
             recruitment_ignore_bad_combat=yes
@@ -61,7 +62,8 @@
             turns=12-FOREVER
             aggression=1.0
             caution=0.0
-            village_value=10
+            leader_value=0
+            village_value=0
             recruitment_pattern=scout,fighter,fighter,mixed fighter
             recruitment_ignore_bad_movement=yes
             recruitment_ignore_bad_combat=yes

src/ai/composite/aspect.hpp
@@ -850,25 +850,7 @@ public:
 
 	virtual bool active() const
 	{
-		if(time_of_day_.empty() == false) {
-			const std::vector<std::string>& times = utils::split(time_of_day_);
-			if(std::count(times.begin(),times.end(),this->get_info().tod_manager_.get_time_of_day().name) == 0) {
-				return false;
-			}
-		}
-
-		if(turns_.empty() == false) {
-			int turn = this->get_info().tod_manager_.turn();
-			const std::vector<std::string>& turns_list = utils::split(turns_);
-			for(std::vector<std::string>::const_iterator j = turns_list.begin(); j != turns_list.end() ; j++ ) {
-				const std::pair<int,int> range = utils::parse_range(*j);
-				if(turn >= range.first && turn <= range.second) {
-				      return true;
-				}
-			}
-			return false;
-		}
-		return true;
+		return this->is_active(time_of_day_,turns_);
 	}
 
 

src/ai/composite/goal.cpp
@@ -31,9 +31,10 @@ static lg::log_domain log_ai_composite_goal("ai/composite/goal");
 #define LOG_AI_COMPOSITE_GOAL LOG_STREAM(info, log_ai_composite_goal)
 #define ERR_AI_COMPOSITE_GOAL LOG_STREAM(err, log_ai_composite_goal)
 
-goal::goal(readonly_context &/*context*/, const config &cfg)
-	: cfg_(cfg), value_()
+goal::goal(readonly_context &context, const config &cfg)
+	: readonly_context_proxy(), cfg_(cfg), value_()
 {
+	init_readonly_context_proxy(context);
 }
 
 
@@ -62,7 +63,10 @@ bool goal::matches_unit(unit_map::const_iterator u)
 	if (!u.valid()) {
 		return false;
 	}
-	config &criteria = cfg_.child("criteria");
+	const config &criteria = cfg_.child("criteria");
+	if (!criteria) {
+		return false;
+	}
 	return u->second.matches_filter(vconfig(criteria),u->first);
 }
 
@@ -84,6 +88,12 @@ bool goal::redeploy(const config &cfg)
 	cfg_ = cfg;
 	on_create();
 	return true;
+}
+
+
+bool goal::active() const
+{
+	return is_active(cfg_["time_of_day"],cfg_["turns"]);
 }
 
 } //end of namespace ai

src/ai/composite/goal.hpp
@@ -19,6 +19,14 @@
 #ifndef AI_COMPOSITE_GOAL_HPP_INCLUDED
 #define AI_COMPOSITE_GOAL_HPP_INCLUDED
 
+
+#ifdef _MSC_VER
+#pragma warning(push)
+//silence "inherits via dominance" warnings
+#pragma warning(disable:4250)
+#endif
+
+
 #include "../../global.hpp"
 
 #include "../contexts.hpp"
@@ -30,10 +38,9 @@
 #include <vector>
 #include <deque>
 
-
 namespace ai {
 
-class goal {
+class goal : public readonly_context_proxy {
 public:
 	goal(readonly_context &context, const config &cfg);
 
@@ -48,6 +55,9 @@ public:
 
 
 	void on_create();
+
+
+	bool active() const;
 
 
 	const std::string& get_id() const;
@@ -111,5 +121,10 @@ public:
 
 
 } //end of namespace ai
+
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
 
 #endif

src/ai/configuration.cpp
@@ -299,6 +299,27 @@ bool configuration::upgrade_side_config_from_1_07_02_to_1_07_03(side_number side
 			}
 			fallback_stage_cfg_ai.append(aiparam);
 		}
+
+		foreach (const config &aitarget, aiparam.child_range("target")) {
+			config aigoal;
+			if (aiparam.has_attribute("turns")) {
+				aigoal["turns"] = aiparam["turns"];
+			}
+			if (aiparam.has_attribute("time_of_day")) {
+				aigoal["time_of_day"] = aiparam["time_of_day"];
+			}
+			if (aitarget.has_attribute("value")) {
+				aigoal["value"] = aitarget["value"];
+			} else {
+				aigoal["value"] = "0";
+			}
+
+			config &aigoalcriteria = aigoal.add_child("criteria",aitarget);
+			aigoalcriteria.remove_attribute("value");
+
+			parsed_cfg.add_child("goal",aigoal);
+		}
+		aiparam.clear_children("target");
 	}
 	fallback_stage_cfg_ai.clear_children("aspect");//@todo 1.7: cleanup only configs of well-known ai aspects
 
@@ -334,13 +355,13 @@ bool configuration::upgrade_side_config_from_1_07_02_to_1_07_03(side_number side
 void configuration::upgrade_aspect_configs_from_1_07_02_to_1_07_03(side_number side, const config::const_child_itors &ai_parameters, config &parsed_cfg)
 {
 	parsed_cfg = config();
-	config cfg("ai");
-	config &cfg_ai = cfg.child("ai");
+	config cfg;
 
 	foreach (const config &aiparam, ai_parameters) {
-		cfg_ai.append(aiparam);
+		cfg.add_child("ai",aiparam);
 	}
-	DBG_AI_CONFIGURATION << "side "<< side <<": upgrading aspects from syntax of 1.7.2. to 1.7.3, old-style config is:" << std::endl << cfg << std::endl;
+
+	DBG_AI_CONFIGURATION << "side "<< side <<": upgrading aspects from syntax of 1.7.2. to 1.7.3, old-style config is:" << std::endl << cfg << std::endl;		
 	foreach (const well_known_aspect &wka, well_known_aspects) {
 		upgrade_aspect_config_from_1_07_02_to_1_07_03(side, cfg,parsed_cfg,wka.name_,wka.was_an_attribute_);
 	}

src/ai/contexts.hpp
@@ -286,6 +286,9 @@ public:
 	virtual int get_villages_per_scout() const = 0;
 
 
+	virtual bool is_active(const std::string &time_of_day, const std::string &turns) const = 0;
+
+
 	virtual void invalidate_defensive_position_cache() const = 0;
 
 
@@ -744,6 +747,13 @@ public:
 	}
 
 
+
+	virtual bool is_active(const std::string &time_of_day, const std::string &turns) const
+	{
+		return target_->is_active(time_of_day, turns);
+	}
+
+
 	virtual void invalidate_defensive_position_cache() const
 	{
 		return target_->invalidate_defensive_position_cache();
@@ -1185,6 +1195,9 @@ public:
 	virtual int get_villages_per_scout() const;
 
 
+	virtual bool is_active(const std::string &time_of_day, const std::string &turns) const;
+
+
 	virtual void invalidate_defensive_position_cache() const;
 
 
@@ -1382,6 +1395,7 @@ public:
 private:
 	recursion_counter recursion_counter_;
 };
+
 
 } //end of namespace ai
 

commit 5a8bd47e30419d77a93a05cf336fe16d00b3c278

Match time_of_day AI aspect against the current ToD id, not its name
This allows ToD-dependent AI configuration in pretty much all cases to
actually work. It seems all mainline WML was coded expecting this aspect
to match the ids, not names, which is only logical since the name
attribute is a user-visible translatable string -- using this for
matching would result in code that only works for certain translations
or lack thereof.

This bug appears to have been first introduced by Crab_ in commit
a14bde5e90941b3358ce66b9a3b9516e0ec17e21 [formerly bb162078a5a0008701061715c260be85f5af1ac3] [formerly ce11fc4cc4ed20e5155bc0fbc8b5cb7f4c1d435e] ("fixed parsing of [target]
tags..."), for version 1.7.4.


Former-commit-id: e6d9ca2796453214de6e9b4842a351becf0ff71a
Former-commit-id: b37ce3c6b8cdc7789789426d793b42af2baeaeb9
Former-commit-id: 9cb061fd4212ac01769af2b4b195c999908aa958
Former-commit-id: 5873e39eee7b6f5d9a563809f38832d5e4ca1e57 [formerly 95be83a6f8048e3c3cf3affd3abefe55bbdb3df5]
Former-commit-id: 2a5cad323a8ed1a9d4b5140c3439859c390dd6b7

src/ai/contexts.cpp
@@ -1302,7 +1302,7 @@ bool readonly_context_impl::is_active(const std::string &time_of_day, const std:
 {
 		if(time_of_day.empty() == false) {
 			const std::vector<std::string>& times = utils::split(time_of_day);
-			if(std::count(times.begin(),times.end(),resources::tod_manager->get_time_of_day().name) == 0) {
+			if(std::count(times.begin(),times.end(),resources::tod_manager->get_time_of_day().id) == 0) {
 				return false;
 			}
 		}