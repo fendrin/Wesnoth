fendrin/Wesnoth

commit 818ffad098059c38ccf3c089bed57a06a29df0fd

allow advanced preference booleans and mp modifications double click toggle.

src/game_preferences_display.cpp
@@ -1078,11 +1078,24 @@ void preferences_dialog::process_event()
 		}
 
 		const config* const adv = get_advanced_pref();
-		if(advanced_button_.pressed()) {
+		const config& pref = *adv;
+		const bool double_click_toggle_boolean = advanced_.double_clicked() &&
+			pref["type"] == "boolean";
+		
+		if(advanced_button_.pressed() || double_click_toggle_boolean) {
+			bool advanced_button_check = advanced_button_.checked();
+			if (double_click_toggle_boolean) {
+				if (advanced_button_.checked()) {
+					advanced_button_check = false;
+				}
+				else {
+					advanced_button_check = true;
+				}
+				advanced_button_.set_check(advanced_button_check);
+			}
 			if(adv != NULL) {
 				const config& pref = *adv;
-				preferences::set(pref["field"],
-						advanced_button_.checked());
+				preferences::set(pref["field"], advanced_button_check);
 				set_advanced_menu();
 
 				if(pref["field"] == "color_cursors") {

src/multiplayer_create.cpp
@@ -262,7 +262,7 @@ void create::process_event()
 	}
 
 	bool update_mod_button_label = mod_selection_ != mods_menu_.selection();
-	if (select_mod_.pressed()) {
+	if (select_mod_.pressed() || mods_menu_.double_clicked()) {
 		int index = mods_menu_.selection();
 		engine_.set_current_mod_index(index);
 		engine_.toggle_current_mod();

commit 09bff2ad754be78c5c7f4cec460d3cb503d92211

Validate a pointer before deferring it.
The problem was introduced in 818ffad0, David Mikos please review this
patch.

Issue found by cppcheck.


Former-commit-id: bc5a7b401a15673a63889823b66791a4cc7d2255
Former-commit-id: 90bbc9aa03eeebacbbd847268b67a99104e5c3ba
Former-commit-id: faedf2d6615fba2c482a4d3a234d0b1ae2332885
Former-commit-id: 0665a23174f5cf85eb178373d2961083c395cf22 [formerly c691e14ece2ea46340035ef84c01ba7b02c5e340]
Former-commit-id: 6bf0559c24bc666cdce61b8ad3498b0f0291968a

src/game_preferences_display.cpp
@@ -1102,10 +1102,10 @@ void preferences_dialog::process_event()
 		}
 
 		const config* const adv = get_advanced_pref();
-		const config& pref = *adv;
-		const bool double_click_toggle_boolean = advanced_.double_clicked() &&
-			pref["type"] == "boolean";
-		
+		const bool double_click_toggle_boolean = adv
+				? advanced_.double_clicked() && (*adv)["type"] == "boolean"
+				: false;
+
 		if(advanced_button_.pressed() || double_click_toggle_boolean) {
 			bool advanced_button_check = advanced_button_.checked();
 			if (double_click_toggle_boolean) {