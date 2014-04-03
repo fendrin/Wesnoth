fendrin/Wesnoth

commit f6d6b1ed488ee41c01a2f5c778efaccf7b12a595

use synced_context: fix mp observers
with the new synced_context system, the old code would bring code in the wrong order onto the replay if we received data during get_user_choice
which now could be called during turn_info::handle_turn.

also previous code would fail to process
[turn]
[command]
[move]
x = 3,4,5
y = 4,3,3
[/move]
[/command]
[/turn]
[turn]
[command]
dependent = yes
[input]
[/input]
[/command]
[/turn]
because [input] would be processed after [move] was processed, but
[move] need [input] on the replay to work properly.
(if luas sync_choice was called from a moveto event)

this commit is part or pr 121.


Former-commit-id: 54acfc0d14f1f9635f00fd261b1630d465c26ac4
Former-commit-id: 68d7d990390fd235303b03c3a161401567232ba5
Former-commit-id: f966041d40299658c4f4bc1d57fa48b21065fc91
Former-commit-id: a1c67e61bbfb87eb80e7fedb8d1abe390e27713d [formerly 30ed66be27840772815a1b02c221f0cd4b83b336]
Former-commit-id: c4c80dc086eee0252e510853190a427c5138181d

src/playmp_controller.cpp
@@ -468,6 +468,16 @@ void playmp_controller::play_network_turn(){
 					break;
 				}
 			}
+			/*
+				we might have data left in replay that we recieved during prestart events. (or maybe other events.)
+			*/
+			else if(!recorder.at_end())
+			{
+				if(do_replay(current_side()))
+				{
+					break;
+				}
+			}
 		}
 
 		play_slice();

src/playturn.cpp
@@ -93,8 +93,11 @@ void turn_info::handle_turn(
 		replay_.append(t);
 		replay_.set_skip(skip_replay);
 
-		turn_end = do_replay(team_num_, &replay_);
+		//turn_end = do_replay(team_num_, &replay_);
+		//note, that this cunfion might call itself recursively: do_replay -> ... -> persist_var -> ... -> handle_generic_event -> sync_network -> handle_turn
 		recorder.add_config(t, replay::MARK_AS_SENT);
+		turn_end = do_replay(team_num_);
+		
 	} else {
 
 		//this turn has finished, so push the remaining moves
@@ -159,8 +162,9 @@ turn_info::PROCESS_DATA_RESULT turn_info::process_network_data(const config& cfg
 
 	BOOST_FOREACH(const config &t, turns)
 	{
-		handle_turn(turn_end, t, skip_replay, backlog);
+		recorder.add_config(t, replay::MARK_AS_SENT);
 	}
+	handle_turn(turn_end, config(), skip_replay, backlog);
 
 	resources::whiteboard->process_network_data(cfg);
 

src/replay.cpp
@@ -809,11 +809,6 @@ void replay::add_config(const config& cfg, MARK_SENT mark)
 	BOOST_FOREACH(const config &cmd, cfg.child_range("command"))
 	{
 		config &cfg = cfg_.add_child("command", cmd);
-		if (cfg.child("speak"))
-		{
-			pos_ = ncommands();
-			add_chat_message_location();
-		}
 		if(mark == MARK_AS_SENT) {
 			cfg["sent"] = true;
 		}
@@ -1405,9 +1400,10 @@ void replay_network_sender::commit_and_sync()
 {
 	if(network::nconnections() > 0) {
 		resources::whiteboard->send_network_data();
-
+		
 		config cfg;
 		const config& data = cfg.add_child("turn",obj_.get_data_range(upto_,obj_.ncommands()));
+		
 		if(data.empty() == false) {
 			network::send_data(cfg, 0);
 		}

commit 296f2c78b36237bcb0a2ad5ffd99c197acd008b1

fix skip_replay part2
This was broken in f6d6b1ed488ee41c01a2f5c778efaccf7b12a595 [formerly c4c80dc086eee0252e510853190a427c5138181d] [formerly a1c67e61bbfb87eb80e7fedb8d1abe390e27713d [formerly 30ed66be27840772815a1b02c221f0cd4b83b336]] [formerly f966041d40299658c4f4bc1d57fa48b21065fc91] [formerly 68d7d990390fd235303b03c3a161401567232ba5] [formerly 54acfc0d14f1f9635f00fd261b1630d465c26ac4].

Former-commit-id: f2cc1e4be47b8ed99e80dcd925ae9c05f7e96090
Former-commit-id: 7b7e438e3f0159cca95ae7b8fbc0e69366aeb38b
Former-commit-id: 9bb26c5e713bf9554bd60fced706315668054881
Former-commit-id: 93f663ecf8888894498eaccf50c2a4e4967db6b6 [formerly 0e58590bffc13bbb6abca0cb5e0c946202dd9d6e]
Former-commit-id: 0c322d7d902b35b2c164d5533296eabe57222aa0

src/playmp_controller.cpp
@@ -469,10 +469,13 @@ void playmp_controller::play_network_turn(){
 			*/
 			else if(!recorder.at_end())
 			{
+				bool was_skipping = recorder.is_skipping();
+				recorder.set_skip(skip_replay_);
 				if(do_replay(current_side()))
 				{
 					break;
 				}
+				recorder.set_skip(was_skipping);
 			}
 		}
 