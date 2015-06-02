fendrin/Wesnoth

commit 202225a5cda35958d25c7bbf5eaabf0f7cea0040

cache minimap images in mp create screen
When we are actually in the game, the image.cpp file uses an
elaborate collection of caches to make sure that all images are
cached and reloaded rather than redoing work (even scaling). It's
questionable whether the amount of caching going on there is
appropriate, it seems it might even be faster if we did less. But
one place that we don't do any caching and performance is bad is
in the mp create screen. There was a bug report in 1.12:

https://gna.org/bugs/?func=detailitem&item_id=21801

In this commit, I implement quick and dirty caching for scenario
minimap images. Each scenario record holds a surface (an SDL
reference counted pointer to image data), and when a minimap is
rendered this gets saved, along with the md5 hash of the map that
got rendered. Whenever a minimap is requested, we hash it first
and check if the cache is dirty. If so we recalculate, otherwise
we return the cached image.

The code is also cleaner and a bit less convoluted than the
previous code -- the previous author seemed to think that surfaces
are "object" types and not "pointer" types, that require an
additional layer of memory management with boost scoped pointer
etc. This was in fact redundant.

I don't know if this commit will actually help report 21801,
the profiling seemed to suggest that it's the minimap rendering
code that is actually slow. But it seems that this can't hurt,
and may at least mitigate the slowness, since except for random
maps it means the minimap calculation won't be repeated.


Former-commit-id: 386196bce1741bd45fd1fd3988a04c07bb77aaf4
Former-commit-id: 8c27d0986c631a0d0c1b6b34f1889381ab4376a5
Former-commit-id: fa577e9d7b9107ab0f106593a5088d7e9ca18efd
Former-commit-id: 77620781fddb38161551c62c2b7867cc1878e158 [formerly 70d6a5d6eeaf89b9188449a130320e90ddc1b5ae]
Former-commit-id: d54a01768867c0e1f9326694e24fbdb1aa14519e

src/game_initialization/create_engine.cpp
@@ -22,6 +22,7 @@
 #include "gui/dialogs/campaign_difficulty.hpp"
 #include "filesystem.hpp"
 #include "formula_string_utils.hpp"
+#include "hash.hpp"
 #include "log.hpp"
 #include "generators/map_create.hpp"
 #include "map_exception.hpp"
@@ -138,16 +139,21 @@ bool scenario::can_launch_game() const
 	return map_.get() != NULL;
 }
 
-surface* scenario::create_image_surface(const SDL_Rect& image_rect)
+surface scenario::create_image_surface(const SDL_Rect& image_rect)
 {
-	surface* minimap = NULL;
-
-	if (map_.get() != NULL) {
-		minimap = new surface(image::getMinimap(image_rect.w,
-			image_rect.h, *map_, 0));
+	if (!map_) {
+		minimap_img_ = surface();
+		return minimap_img_;
 	}
 
-	return minimap;
+	std::basic_string<unsigned char> current_hash = util::md5(map_->write());
+
+	if (minimap_img_.null() || (map_hash_ != current_hash)) { // If there's no minimap image, or the map hash doesn't match, regenerate the image cache.
+		minimap_img_ = image::getMinimap(image_rect.w, image_rect.h, *map_, 0);
+		map_hash_ = current_hash;
+	}
+
+	return minimap_img_;
 }
 
 void scenario::set_metadata()
@@ -333,15 +339,12 @@ bool campaign::can_launch_game() const
 	return !data_.empty();
 }
 
-surface* campaign::create_image_surface(const SDL_Rect& image_rect)
+surface campaign::create_image_surface(const SDL_Rect& image_rect)
 {
 	surface temp_image(
 		image::get_image(image::locator(image_label_)));
 
-	surface* campaign_image = new surface(scale_surface(temp_image,
-		image_rect.w, image_rect.h));
-
-	return campaign_image;
+	return scale_surface(temp_image, image_rect.w, image_rect.h);
 }
 
 void campaign::set_metadata()

src/game_initialization/create_engine.hpp
@@ -37,7 +37,7 @@ public:
 
 	virtual bool can_launch_game() const = 0;
 
-	virtual surface* create_image_surface(const SDL_Rect& image_rect) = 0;
+	virtual surface create_image_surface(const SDL_Rect& image_rect) = 0;
 
 	virtual void set_metadata() = 0;
 
@@ -67,7 +67,7 @@ public:
 
 	bool can_launch_game() const;
 
-	surface* create_image_surface(const SDL_Rect& image_rect);
+	surface create_image_surface(const SDL_Rect& image_rect);
 
 	void set_metadata();
 
@@ -78,6 +78,9 @@ protected:
 	void set_sides();
 
 	boost::scoped_ptr<gamemap> map_;
+
+	surface minimap_img_;
+	std::basic_string<unsigned char> map_hash_;
 
 private:
 	scenario(const scenario&);
@@ -140,7 +143,7 @@ public:
 
 	bool can_launch_game() const;
 
-	surface* create_image_surface(const SDL_Rect& image_rect);
+	surface create_image_surface(const SDL_Rect& image_rect);
 
 	void set_metadata();
 

src/game_initialization/multiplayer_create.cpp
@@ -493,19 +493,18 @@ void create::synchronize_selections()
 
 void create::draw_level_image()
 {
-	boost::scoped_ptr<surface> image(
+	surface image(
 		engine_.current_level().create_image_surface(image_rect_));
 
-	if (image.get() != NULL) {
+	if (!image.null()) {
 		SDL_Color back_color = {0,0,0,255};
-		draw_centered_on_background(*image, image_rect_, back_color,
+		draw_centered_on_background(image, image_rect_, back_color,
 			video().getSurface());
 	} else {
 		surface display(disp_.get_screen_surface());
 		sdl::fill_rect(display, &image_rect_,
 			SDL_MapRGB(display->format, 0, 0, 0));
 		update_rect(image_rect_);
-
 	}
 }
 

commit 461230d41432645c6c59411be15f7b972defc0ad

Fix unbound memory read (bug #23606)
This was introduced in commit 202225a5cda35958d25c7bbf5eaabf0f7cea0040 [formerly d54a01768867c0e1f9326694e24fbdb1aa14519e] [formerly 77620781fddb38161551c62c2b7867cc1878e158 [formerly 70d6a5d6eeaf89b9188449a130320e90ddc1b5ae]] [formerly fa577e9d7b9107ab0f106593a5088d7e9ca18efd] [formerly 8c27d0986c631a0d0c1b6b34f1889381ab4376a5] [formerly 386196bce1741bd45fd1fd3988a04c07bb77aaf4],
probably because it's not clear enough in our implementation of MD5 that
the result of MD5::raw_digest() (and util::md5() by proxy) is not a
null-terminated string. I've added a comment to util::md5() to clarify
this in case it comes up again, but what we really should do is replace
the MD5 implementation with something a bit more C++-ish.

Fixed the issue by having the caller request the text representation of
the MD5 digest instead.


Former-commit-id: fe644aedc6652d58e8803352542b8471c2bb0369
Former-commit-id: 940ffe004bb62cd8db0ca6b03da3b6591252a5d5
Former-commit-id: aec4ead58b0b200b7b45095f79909848257b8cba
Former-commit-id: ec01f87a4074bde20bc977a18ba5b821c43a5732 [formerly 377706dc18646e09e06e22cd6f2fa7ab77e40c3d]
Former-commit-id: 644c810cb94b673696fbaeef88d96edeca5508a6

src/game_initialization/create_engine.cpp
@@ -148,7 +148,7 @@ surface scenario::create_image_surface(const SDL_Rect& image_rect)
 		return minimap_img_;
 	}
 
-	std::basic_string<unsigned char> current_hash = util::md5(map_->write());
+	std::string current_hash = util::encode_hash(util::md5(map_->write()));
 
 	if (minimap_img_.null() || (map_hash_ != current_hash)) { // If there's no minimap image, or the map hash doesn't match, regenerate the image cache.
 		minimap_img_ = image::getMinimap(image_rect.w, image_rect.h, *map_, 0);

src/game_initialization/create_engine.hpp
@@ -89,7 +89,7 @@ protected:
 	boost::scoped_ptr<gamemap> map_;
 
 	surface minimap_img_;
-	std::basic_string<unsigned char> map_hash_;
+	std::string map_hash_;
 
 private:
 	scenario(const scenario&);

src/hash.hpp
@@ -19,6 +19,13 @@
 
 namespace util {
 
+/**
+ * Returns the MD5 digest for the specified input.
+ *
+ * @note The returned value points to a fixed-size 16 bytes array representing
+ *       the raw MD5 value, not a null-terminated string. Use encode_hash if
+ *       you need the text representation instead.
+ */
 unsigned char* md5(const std::string& input);
 int get_iteration_count(const std::string& hash);
 std::string get_salt(const std::string& hash);