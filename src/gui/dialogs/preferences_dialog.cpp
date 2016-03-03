fendrin/Wesnoth

commit 5515ccdfa84f56905e69c9247338569e2f275bee

tpreferences: ensure all descriptions are shown
Former-commit-id: 91f9ac5b6669992a1dd4ced35b5d2b3260a736a9
Former-commit-id: 1510157d2c2169f187582a9fb1f1002eeb36f882
Former-commit-id: aea29075f9a9c45f0bca238a416859c1f8e8cbf9
Former-commit-id: 7c84397654c34ef8a8294422287f80ce4f70e5cd [formerly fe1a66df2c6b12e33d954b5c9158664fa16e1f78]
Former-commit-id: bc5bafd6ef21b05f41be73b77596458ef48d5358

src/gui/dialogs/preferences_dialog.cpp
@@ -851,7 +851,9 @@ void tpreferences::on_advanced_prefs_list_select(tlistbox& list, twindow& window
 		// Add more options here as needed
 	}
 
-	if(selected_type != ADVANCED_PREF_TYPE::TOGGLE && selected_type != ADVANCED_PREF_TYPE::SPECIAL) {
+	const bool has_description = !adv_preferences_cfg_[selected_row]["description"].empty();
+
+	if(has_description && selected_type != ADVANCED_PREF_TYPE::SPECIAL) {
 		find_widget<twidget>(get_advanced_row_grid(list, selected_row), "prefs_setter_grid", false)
 			.set_visible(tcontrol::tvisible::visible);
 	}

commit 276e0f6667eb0db3c6a3fc7f57771ef8836f6d82

tpreferences: fixed regression where certain advanced options weren't opening
This was accidentally broken in 5515ccdfa84f56905e69c9247338569e2f275bee [formerly bc5bafd6ef21b05f41be73b77596458ef48d5358] [formerly 7c84397654c34ef8a8294422287f80ce4f70e5cd [formerly fe1a66df2c6b12e33d954b5c9158664fa16e1f78]] [formerly aea29075f9a9c45f0bca238a416859c1f8e8cbf9] [formerly 1510157d2c2169f187582a9fb1f1002eeb36f882] [formerly 91f9ac5b6669992a1dd4ced35b5d2b3260a736a9]


Former-commit-id: cda780278f3c9bcb867c6520581388b062d4babe
Former-commit-id: 0f1ca7b249af451fccc23db84faab9f2e89fa9f2
Former-commit-id: 4d1cec23d17b20765012bd9529c2795b2218dfbd
Former-commit-id: 8a9fc8b36d894333b3cc43d08621f1e7160829e9 [formerly 0ce6e839497c9af225935f006491b954c17ebbf9]
Former-commit-id: d3584564f0893fbe34de9720f1dfdbef381627c4

src/gui/dialogs/preferences_dialog.cpp
@@ -1046,7 +1046,7 @@ void tpreferences::on_advanced_prefs_list_select(tlistbox& list, twindow& window
 
 	const bool has_description = !adv_preferences_cfg_[selected_row]["description"].empty();
 
-	if(has_description && selected_type != ADVANCED_PREF_TYPE::SPECIAL) {
+	if(has_description || (selected_type != ADVANCED_PREF_TYPE::SPECIAL && selected_type != ADVANCED_PREF_TYPE::TOGGLE)) {
 		find_widget<twidget>(get_advanced_row_grid(list, selected_row), "prefs_setter_grid", false)
 			.set_visible(tcontrol::tvisible::visible);
 	}