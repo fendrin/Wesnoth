fendrin/Wesnoth

commit fff29ab5adda017cc1c0bd51a74e47b9e24177ec

Use std::to_string() or equivalent wherever possible
This commit removes the utility srt_cast() function and replaces its calls,
along with calls to lexical_cast<std::string>() (and its boost variant),
with std::to_string().

In a few cases where the input type isn't compatible with to_string,
lexical_cast<std::string> is still used.

In other cases where lexical_cast was operating on MAKE_ENUM enums, the
call has been replaced with ENUM::enum_to_string, which is faster.


Former-commit-id: f3f1aa74bf807ea2e735ba821433b75b6239e9dd
Former-commit-id: ca75a4d7f1c1f5454f153433a1875d5feaaf2e08
Former-commit-id: ac7351426da322a6e15bc42f0cad4f8b0c3d189e
Former-commit-id: a9e0aeeaf8a855aa7ef5d77c89b47c4755857516 [formerly 6840c76c3feb62027c8371180a63eb8c1d48f2e1]
Former-commit-id: 3b04798293f173e124ef23f1e45475084a4b204f

src/game_initialization/create_engine.cpp
@@ -209,7 +209,7 @@ void scenario::set_sides()
 			pos < map_positions; ++pos) {
 			config& side = data_.add_child("side");
 			side["side"] = pos + 1;
-			side["team_name"] = "Team " + lexical_cast<std::string>(pos + 1);
+			side["team_name"] = "Team " + std::to_string(pos + 1);
 			side["canrecruit"] = true;
 			side["controller"] = "human";
 		}
@@ -1059,7 +1059,7 @@ void create_engine::init_all_levels()
 	// Stand-alone scenarios.
 	BOOST_FOREACH(const config &data,
 		game_config_manager::get()->game_config().child_range(
-		lexical_cast<std::string> (game_classification::CAMPAIGN_TYPE::MULTIPLAYER)))
+		std::to_string (game_classification::CAMPAIGN_TYPE::MULTIPLAYER)))
 	{
 		if (!data["allow_new_game"].to_bool(true))
 			continue;

commit 895d1d0f25303202cfb007c32e6c84e38e21fad1

Fixed initializing of mp scenarios
Was accidentally broken in fff29ab5add [formerly 3b04798293f] [formerly a9e0aeeaf8a [formerly 6840c76c3fe]] [formerly ac7351426da] [formerly ca75a4d7f1c] [formerly f3f1aa74bf8].


Former-commit-id: 369f940bf4bdf796e6eb002a3db86a6cde69c37a
Former-commit-id: 89ab5fff3dbdcb50e81daa7c64c18c84312032d0
Former-commit-id: 7abf5aad0fd76a4e7ec47a76d20cd23ba9600b1d
Former-commit-id: c1ea4202821851af544be57cf40bbf477b450922 [formerly 6604516691e9accb77b3dc099783e7c16b88f73f]
Former-commit-id: 19850842e95ca7d0a0f733d64e786469cc61ae90

src/game_initialization/create_engine.cpp
@@ -1058,8 +1058,7 @@ void create_engine::init_all_levels()
 
 	// Stand-alone scenarios.
 	BOOST_FOREACH(const config &data,
-		game_config_manager::get()->game_config().child_range(
-		std::to_string (game_classification::CAMPAIGN_TYPE::MULTIPLAYER)))
+		game_config_manager::get()->game_config().child_range("multiplayer"))
 	{
 		if (!data["allow_new_game"].to_bool(true))
 			continue;