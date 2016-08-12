fendrin/Wesnoth

commit b351821bcbe5285b2c02d3d96f576be205b6a0d3

Replace resources::teams with resources::gameboard->teams()
Former-commit-id: b4af91f68fa4723cb2f7729285f04d5a41020ee5
Former-commit-id: bd9635e9a65c54245739680ec2a646ca9e5f3e37
Former-commit-id: 606a0eda79200de4ac422d9a37f978433aacf00e
Former-commit-id: 8ae4ce7f8ee69c7608ffe71c0453125204d73df6 [formerly 8c98970ae508696640ca1b0bd5046330aea2a570]
Former-commit-id: 5bc58ddd9064f5e44e95cc9ffdeb6af3a7e302e6

src/editor/map/context_manager.cpp
@@ -21,6 +21,7 @@
 #include "filesystem.hpp"
 #include "filechooser.hpp"
 #include "formula/string_utils.hpp"
+#include "game_board.hpp"
 #include "gettext.hpp"
 #include "generators/map_create.hpp"
 #include "generators/map_generator.hpp"
@@ -76,7 +77,7 @@ public:
 		context_manager_.gui().change_display_context(&context_manager_.get_map_context());
 
 		resources::units = &context_manager_.get_map_context().get_units();
-		resources::teams = &context_manager_.get_map_context().get_teams();
+		resources::gameboard->teams() = context_manager_.get_map_context().get_teams();
 
 		// TODO register the tod_manager with the gui?
 		resources::tod_manager = context_manager_.get_map_context().get_time_manager();

commit 4cb37ee3a5423755bfe56ef83501d8f627d70e2a

Fixed crash on entering the editor (bug #24964)
This was introduced in b351821bcbe5 [formerly 5bc58ddd9064] [formerly 8ae4ce7f8ee6 [formerly 8c98970ae508]] [formerly 606a0eda7920] [formerly bd9635e9a65c] [formerly b4af91f68fa4].


Former-commit-id: dee7f757ea61422bb00e87bfb982e75c847cb375
Former-commit-id: 6cd4085c4c869708f1fdaddd63fe8d0453dabb19
Former-commit-id: 1356dd935ae02327c424d482f1065dccf02011dd
Former-commit-id: 2acc00d45f4cfe50818689713b49b0edd9a302b5 [formerly 7e797c5d1c679a17e0afccbd3b31c7598741b5d2]
Former-commit-id: 29bd9d69423e1e018bfb7a1181bb992ade9ff489

src/editor/map/context_manager.cpp
@@ -77,7 +77,10 @@ public:
 		context_manager_.gui().change_display_context(&context_manager_.get_map_context());
 
 		resources::units = &context_manager_.get_map_context().get_units();
-		resources::gameboard->teams() = context_manager_.get_map_context().get_teams();
+
+		if(resources::gameboard) {
+			resources::gameboard->teams() = context_manager_.get_map_context().get_teams();
+		}
 
 		// TODO register the tod_manager with the gui?
 		resources::tod_manager = context_manager_.get_map_context().get_time_manager();