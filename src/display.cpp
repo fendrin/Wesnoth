fendrin/Wesnoth

commit 63158f335e888e2f1882b10b0fed9051df1dcfe4

Fix icons of buttons under the minimap disappearing on closing a menu
Former-commit-id: a184d89a9e4043ecc4164458fc2c75eef97ec103
Former-commit-id: 74eb3ed143ff8954721502c54f25bd4ef4cd4b2b
Former-commit-id: dd4d3f6bfcc25810077d506f6af10c93ce738427
Former-commit-id: bd2dc3a8f04643b08b6782331f51acda842f5f60 [formerly de2103a256c349e5c15c3d9f0c227b4ebaf9ea63]
Former-commit-id: 0ab639d0ac3739482f3b857d3cea0e6f879f453d

src/display.cpp
@@ -24,6 +24,7 @@
 #include "game_preferences.hpp"
 #include "gettext.hpp"
 #include "halo.hpp"
+#include "hotkey/command_executor.hpp"
 #include "language.hpp"
 #include "log.hpp"
 #include "marked-up_text.hpp"
@@ -2672,6 +2673,15 @@ void display::redraw_everything()
 
 	if(!menu_buttons_.empty() || !action_buttons_.empty() || !sliders_.empty() ) {
 		create_buttons();
+	}
+
+	hotkey::command_executor* command_executor =
+		resources::controller->get_hotkey_command_executor();
+	if (command_executor != nullptr)
+	{
+		// This function adds button overlays,
+		// it needs to be run after recreating the buttons.
+		command_executor->set_button_state();
 	}
 
 	panelsDrawn_ = false;

src/controller_base.hpp
@@ -70,6 +70,11 @@ public:
 	void set_scroll_down(bool on);
 	void set_scroll_left(bool on);
 	void set_scroll_right(bool on);
+
+	/**
+	* Get (optionally) a command executor to handle context menu events
+	*/
+	virtual hotkey::command_executor * get_hotkey_command_executor() { return nullptr; }
 protected:
 	virtual bool is_browsing() const
 	{ return false; }
@@ -91,11 +96,6 @@ protected:
 	 * Get (optionally) a plugins context a derived class uses
 	 */
 	virtual plugins_context * get_plugins_context() { return nullptr; }
-
-	/**
-	 * Get (optionally) a command executor to handle context menu events
-	 */
-	virtual hotkey::command_executor * get_hotkey_command_executor() { return nullptr; }
 
 	/**
 	 * Derived classes should override this to return false when arrow keys

commit 2038583983ca18723d669d0ed1e2e259b44ccf2f

Fix crash when starting editor introduced in 63158f335e88 [formerly 0ab639d0ac37] [formerly bd2dc3a8f046 [formerly de2103a256c3]] [formerly dd4d3f6bfcc2] [formerly 74eb3ed143ff] [formerly a184d89a9e40]
Former-commit-id: 175e1526ac2600d441d8c6fa0d958fa0a629e60b
Former-commit-id: dd9017670b974c50a74d0b63c8b6640347af650b
Former-commit-id: 39080ee50305550aea9ac49e67130f35fa5a107b
Former-commit-id: b99dc8b98428ce732fbc78e467cf515d6fe98018 [formerly 199e9f77dd053235a8e4f31494106b94d6b8b8d3]
Former-commit-id: 51e0db7a54c13178d13af96c63d3fba788587874

src/display.cpp
@@ -2675,13 +2675,13 @@ void display::redraw_everything()
 		create_buttons();
 	}
 
-	hotkey::command_executor* command_executor =
-		resources::controller->get_hotkey_command_executor();
-	if (command_executor != nullptr)
-	{
-		// This function adds button overlays,
-		// it needs to be run after recreating the buttons.
-		command_executor->set_button_state();
+	if(resources::controller) {
+		hotkey::command_executor* command_executor = resources::controller->get_hotkey_command_executor();
+		if(command_executor != nullptr)	{
+			// This function adds button overlays,
+			// it needs to be run after recreating the buttons.
+			command_executor->set_button_state();
+		}
 	}
 
 	panelsDrawn_ = false;