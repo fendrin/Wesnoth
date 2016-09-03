fendrin/Wesnoth

commit ec7f8d7cf530eb15c83c0f4ea6d34b059dad89a9

Default Map Generator: refactored generator setting parameters into a struct
Former-commit-id: 6107a58f2c9a8be58a77bc9e88a192b6e0efc2b9
Former-commit-id: 98bbc747c1c4a4e6ce5eaf4eff32d3fd172c2ba5
Former-commit-id: 3fbb3aeed6770c0b7b71890058692fc93413abda
Former-commit-id: f63439f467f2eabdb6f308a9ca821fbd32d19929 [formerly e2c4310a5d12bde180392cd5d65d7332d2f6224e]
Former-commit-id: f52e9a96c5ae6e4e273a62847fa41e61b49e2f62

src/generators/default_map_generator.cpp
@@ -36,62 +36,27 @@ namespace {
 	const size_t max_coastal = 5;
 }
 
-default_map_generator::default_map_generator(const config &cfg) :
-	default_width_(40),
-	default_height_(40),
-	width_(40),
-	height_(40),
-	island_size_(0),
-	iterations_(1000),
-	hill_size_(10),
-	max_lakes_(20),
-	nvillages_(25),
-	castle_size_(9),
-	nplayers_(2),
-	link_castles_(true),
-	show_labels_(true),
-	cfg_(cfg ? cfg : config())
+generator_data::generator_data(const config &cfg)
+	: width(std::max(0, cfg["map_width"].to_int(40)))
+	, height(std::max(0, cfg["map_height"].to_int(40)))
+	, default_width(width)
+	, default_height(height)
+	, nplayers(std::max(0, cfg["players"].to_int(2)))
+	, nvillages(std::max(0, cfg["villages"].to_int(25)))
+	, iterations(std::max(0, cfg["iterations"].to_int(1000)))
+	, hill_size(std::max(0, cfg["hill_size"].to_int(10)))
+	, castle_size(std::max(0, cfg["castle_size"].to_int(9)))
+	, island_size(std::max(0, cfg["island_size"].to_int(0)))
+	, max_lakes(std::max(0, cfg["max_lakes"].to_int(20)))
+	, link_castles(true)
+	, show_labels(true)
 {
-	if (!cfg) return;
+}
 
-	int width = cfg["map_width"];
-	if (width > 0)
-		width_ = width;
-
-	int height = cfg["map_height"];
-	if (height > 0)
-		height_ = height;
-
-	default_width_ = width_;
-	default_height_ = height_;
-
-	int iterations = cfg["iterations"];
-	if (iterations > 0)
-		iterations_ = iterations;
-
-	int hill_size = cfg["hill_size"];
-	if (hill_size > 0)
-		hill_size_ = hill_size;
-
-	int max_lakes = cfg["max_lakes"];
-	if (max_lakes > 0)
-		max_lakes_ = max_lakes;
-
-	int nvillages = cfg["villages"];
-	if (nvillages > 0)
-		nvillages_ = nvillages;
-
-	int castle_size = cfg["castle_size"];
-	if (castle_size > 0)
-		castle_size_ = castle_size;
-
-	int nplayers = cfg["players"];
-	if (nplayers > 0)
-		nplayers_ = nplayers;
-
-	int island_size = cfg["island_size"];
-	if (island_size > 0)
-		island_size_ = island_size;
+default_map_generator::default_map_generator(const config& cfg)
+	: cfg_(cfg)
+	, data_(cfg)
+{
 }
 
 bool default_map_generator::allow_user_config() const { return true; }
@@ -129,7 +94,7 @@ void default_map_generator::user_config(CVideo& v)
 	SDL_Rect players_rect = font::draw_text(nullptr,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,players_label,0,0);
 	SDL_Rect width_rect = font::draw_text(nullptr,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,width_label,0,0);
 	SDL_Rect height_rect = font::draw_text(nullptr,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,height_label,0,0);
-	SDL_Rect iterations_rect = font::draw_text(nullptr,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,iterations_label,0,0);
+	SDL_Rect iteration_rect = font::draw_text(nullptr,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,iterations_label,0,0);
 	SDL_Rect hillsize_rect = font::draw_text(nullptr,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,hillsize_label,0,0);
 	SDL_Rect villages_rect = font::draw_text(nullptr,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,villages_label,0,0);
 	SDL_Rect castlesize_rect = font::draw_text(nullptr,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,castlesize_label,0,0);
@@ -138,12 +103,12 @@ void default_map_generator::user_config(CVideo& v)
 	const int horz_margin = 15;
 	const int text_right = xpos + horz_margin +
 	        std::max<int>(std::max<int>(std::max<int>(std::max<int>(std::max<int>(std::max<int>(
-		         players_rect.w,width_rect.w),height_rect.w),iterations_rect.w),hillsize_rect.w),villages_rect.w),castlesize_rect.w);
+		         players_rect.w,width_rect.w),height_rect.w),iteration_rect.w),hillsize_rect.w),villages_rect.w),castlesize_rect.w);
 
 	players_rect.x = text_right - players_rect.w;
 	width_rect.x = text_right - width_rect.w;
 	height_rect.x = text_right - height_rect.w;
-	iterations_rect.x = text_right - iterations_rect.w;
+	iteration_rect.x = text_right - iteration_rect.w;
 	hillsize_rect.x = text_right - hillsize_rect.w;
 	villages_rect.x = text_right - villages_rect.w;
 	castlesize_rect.x = text_right - castlesize_rect.w;
@@ -153,10 +118,10 @@ void default_map_generator::user_config(CVideo& v)
 	players_rect.y = ypos + vertical_margin*2;
 	width_rect.y = players_rect.y + players_rect.h + vertical_margin;
 	height_rect.y = width_rect.y + width_rect.h + vertical_margin;
-	iterations_rect.y = height_rect.y + height_rect.h + vertical_margin;
-	hillsize_rect.y = iterations_rect.y + iterations_rect.h + vertical_margin;
+	iteration_rect.y = height_rect.y + height_rect.h + vertical_margin;
+	hillsize_rect.y = iteration_rect.y + iteration_rect.h + vertical_margin;
 	villages_rect.y = hillsize_rect.y + hillsize_rect.h + vertical_margin;
-	castlesize_rect.y = villages_rect.y + iterations_rect.h + vertical_margin;
+	castlesize_rect.y = villages_rect.y + iteration_rect.h + vertical_margin;
 	landform_rect.y = castlesize_rect.y + villages_rect.h + vertical_margin;
 
 	const int right_space = 150;
@@ -172,7 +137,7 @@ void default_map_generator::user_config(CVideo& v)
 	players_slider.set_location(slider_rect);
 	players_slider.set_min(2);
 	players_slider.set_max(gamemap::MAX_PLAYERS);
-	players_slider.set_value(nplayers_);
+	players_slider.set_value(data_.nplayers);
 
 	const int min_width = 20;
 	const int max_width = 100;
@@ -184,24 +149,24 @@ void default_map_generator::user_config(CVideo& v)
 	width_slider.set_location(slider_rect);
 	width_slider.set_min(min_width+(players_slider.value()-2)*extra_size_per_player);
 	width_slider.set_max(max_width);
-	width_slider.set_value(width_);
+	width_slider.set_value(data_.width);
 
 	slider_rect.y = height_rect.y;
 	gui::slider height_slider(screen);
 	height_slider.set_location(slider_rect);
 	height_slider.set_min(min_width+(players_slider.value()-2)*extra_size_per_player);
 	height_slider.set_max(max_height);
-	height_slider.set_value(height_);
+	height_slider.set_value(data_.height);
 
 	const int min_iterations = 10;
 	const int max_iterations = 3000;
 
-	slider_rect.y = iterations_rect.y;
+	slider_rect.y = iteration_rect.y;
 	gui::slider iterations_slider(screen);
 	iterations_slider.set_location(slider_rect);
 	iterations_slider.set_min(min_iterations);
 	iterations_slider.set_max(max_iterations);
-	iterations_slider.set_value(iterations_);
+	iterations_slider.set_value(data_.iterations);
 
 	const int min_hillsize = 1;
 	const int max_hillsize = 50;
@@ -211,7 +176,7 @@ void default_map_generator::user_config(CVideo& v)
 	hillsize_slider.set_location(slider_rect);
 	hillsize_slider.set_min(min_hillsize);
 	hillsize_slider.set_max(max_hillsize);
-	hillsize_slider.set_value(hill_size_);
+	hillsize_slider.set_value(data_.hill_size);
 
 	const int min_villages = 0;
 	const int max_villages = 50;
@@ -221,7 +186,7 @@ void default_map_generator::user_config(CVideo& v)
 	villages_slider.set_location(slider_rect);
 	villages_slider.set_min(min_villages);
 	villages_slider.set_max(max_villages);
-	villages_slider.set_value(nvillages_);
+	villages_slider.set_value(data_.nvillages);
 
 	const int min_castlesize = 2;
 	const int max_castlesize = 14;
@@ -231,7 +196,7 @@ void default_map_generator::user_config(CVideo& v)
 	castlesize_slider.set_location(slider_rect);
 	castlesize_slider.set_min(min_castlesize);
 	castlesize_slider.set_max(max_castlesize);
-	castlesize_slider.set_value(castle_size_);
+	castlesize_slider.set_value(data_.castle_size);
 
 
 	const int min_landform = 0;
@@ -241,31 +206,31 @@ void default_map_generator::user_config(CVideo& v)
 	landform_slider.set_location(slider_rect);
 	landform_slider.set_min(min_landform);
 	landform_slider.set_max(max_landform);
-	landform_slider.set_value(island_size_);
+	landform_slider.set_value(data_.island_size);
 
 	SDL_Rect link_rect = slider_rect;
 	link_rect.y = link_rect.y + link_rect.h + vertical_margin;
 
 	gui::button link_castles(screen,_("Roads between castles"),gui::button::TYPE_CHECK);
-	link_castles.set_check(link_castles_);
+	link_castles.set_check(data_.link_castles);
 	link_castles.set_location(link_rect);
 
 	SDL_Rect labels_rect = link_rect;
 	labels_rect.y = labels_rect.y + labels_rect.h + vertical_margin;
 
 	gui::button show_labels(screen,_("Show labels"),gui::button::TYPE_CHECK);
-	show_labels.set_check(show_labels_);
+	show_labels.set_check(data_.show_labels);
 	show_labels.set_location(labels_rect);
 
 	while(true) {
-		nplayers_ = players_slider.value();
-		width_ = width_slider.value();
-		height_ = height_slider.value();
-		iterations_ = iterations_slider.value();
-		hill_size_ = hillsize_slider.value();
-		nvillages_ = villages_slider.value();
-		castle_size_ = castlesize_slider.value();
-		island_size_ = landform_slider.value();
+		data_.nplayers = players_slider.value();
+		data_.width = width_slider.value();
+		data_.height = height_slider.value();
+		data_.iterations = iterations_slider.value();
+		data_.hill_size = hillsize_slider.value();
+		data_.nvillages = villages_slider.value();
+		data_.castle_size = castlesize_slider.value();
+		data_.island_size = landform_slider.value();
 
 		dialog_restorer.restore();
 		close_button.set_dirty(true);
@@ -293,35 +258,35 @@ void default_map_generator::user_config(CVideo& v)
 		font::draw_text(&screen,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,players_label,players_rect.x,players_rect.y);
 		font::draw_text(&screen,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,width_label,width_rect.x,width_rect.y);
 		font::draw_text(&screen,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,height_label,height_rect.x,height_rect.y);
-		font::draw_text(&screen,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,iterations_label,iterations_rect.x,iterations_rect.y);
+		font::draw_text(&screen,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,iterations_label,iteration_rect.x,iteration_rect.y);
 		font::draw_text(&screen,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,hillsize_label,hillsize_rect.x,hillsize_rect.y);
 		font::draw_text(&screen,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,villages_label,villages_rect.x,villages_rect.y);
 		font::draw_text(&screen,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,castlesize_label,castlesize_rect.x,castlesize_rect.y);
 		font::draw_text(&screen,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,landform_label,landform_rect.x,landform_rect.y);
 
 		font::draw_text(&screen, screen_area(), font::SIZE_NORMAL,
-			font::NORMAL_COLOR, std::to_string(nplayers_),
+			font::NORMAL_COLOR, std::to_string(data_.nplayers),
 			slider_right + horz_margin, players_rect.y);
 
 		font::draw_text(&screen, screen_area(), font::SIZE_NORMAL,
-			font::NORMAL_COLOR, std::to_string(width_),
+			font::NORMAL_COLOR, std::to_string(data_.width),
 			slider_right + horz_margin, width_rect.y);
 
 		font::draw_text(&screen, screen_area(), font::SIZE_NORMAL,
-			font::NORMAL_COLOR, std::to_string(height_),
+			font::NORMAL_COLOR, std::to_string(data_.height),
 			slider_right+horz_margin,height_rect.y);
 
 		std::stringstream villages_str;
-		villages_str << nvillages_ << _("/1000 tiles");
+		villages_str << data_.nvillages << _("/1000 tiles");
 		font::draw_text(&screen,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,villages_str.str(),
 		                slider_right+horz_margin,villages_rect.y);
 
 		font::draw_text(&screen, screen_area(), font::SIZE_NORMAL,
-			font::NORMAL_COLOR, std::to_string(castle_size_),
+			font::NORMAL_COLOR, std::to_string(data_.castle_size),
 			slider_right + horz_margin, castlesize_rect.y);
 
 		std::stringstream landform_str;
-		landform_str << translation::gettext(island_size_ == 0 ? N_("Inland") : (island_size_ < max_coastal ? N_("Coastal") : N_("Island")));
+		landform_str << translation::gettext(data_.island_size == 0 ? N_("Inland") : (data_.island_size < max_coastal ? N_("Coastal") : N_("Island")));
 		font::draw_text(&screen,screen_area(),font::SIZE_NORMAL,font::NORMAL_COLOR,landform_str.str(),
 			            slider_right+horz_margin,landform_rect.y);
 
@@ -332,8 +297,8 @@ void default_map_generator::user_config(CVideo& v)
 		events::pump();
 	}
 
-	link_castles_ = link_castles.checked();
-	show_labels_ = show_labels.checked();
+	data_.link_castles = link_castles.checked();
+	data_.show_labels = show_labels.checked();
 }
 
 std::string default_map_generator::name() const { return "default"; }
@@ -362,35 +327,36 @@ std::string default_map_generator::generate_map(std::map<map_location,std::strin
 	}
 
 	// Suppress labels?
-	if ( !show_labels_ )
+	if ( !data_.show_labels )
 		labels = nullptr;
 
 	// the random generator thinks odd widths are nasty, so make them even
-	if (is_odd(width_))
-		++width_;
+	if (is_odd(data_.width))
+		++data_.width;
 
-	size_t iterations = (iterations_*width_*height_)/(default_width_*default_height_);
-	size_t island_size = 0;
+	data_.island_size = 0;
+	data_.iterations = (data_.iterations*data_.width*data_.height)/(data_.default_width*data_.default_height);
 	size_t island_off_center = 0;
-	size_t max_lakes = max_lakes_;
 
-	if(island_size_ >= max_coastal) {
+	if(static_cast<unsigned>(data_.island_size) >= max_coastal) {
 
 		//islands look good with much fewer iterations than normal, and fewer lake
-		iterations /= 10;
-		max_lakes /= 9;
+		data_.iterations /= 10;
+		data_.max_lakes /= 9;
 
 		//the radius of the island should be up to half the width of the map
-		const size_t island_radius = 50 + ((max_island - island_size_)*50)/(max_island - max_coastal);
-		island_size = (island_radius*(width_/2))/100;
-	} else if(island_size_ > 0) {
+		const size_t island_radius = 50 + ((max_island - data_.island_size)*50)/(max_island - max_coastal);
+		data_.island_size = (island_radius*(data_.width/2))/100;
+	} else if(data_.island_size > 0) {
 		DBG_NG << "coastal...\n";
 		//the radius of the island should be up to twice the width of the map
-		const size_t island_radius = 40 + ((max_coastal - island_size_)*40)/max_coastal;
-		island_size = (island_radius*width_*2)/100;
-		island_off_center = std::min<size_t>(width_,height_);
+		const size_t island_radius = 40 + ((max_coastal - data_.island_size)*40)/max_coastal;
+		data_.island_size = (island_radius*data_.width*2)/100;
+		island_off_center = std::min<size_t>(data_.width,data_.height);
 		DBG_NG << "calculated coastal params...\n";
 	}
+
+	data_.nvillages = (data_.nvillages * data_.width * data_.height) / 1000;
 
 	// A map generator can fail so try a few times to get a map before aborting.
 	std::string map;
@@ -407,9 +373,7 @@ std::string default_map_generator::generate_map(std::map<map_location,std::strin
 			labels_copy = *labels;
 		}
 		try{
-			map = job.default_generate_map(width_, height_, island_size, island_off_center,
-				iterations, hill_size_, max_lakes, (nvillages_ * width_ * height_) / 1000,
-				castle_size_, nplayers_, link_castles_, labels_ptr, cfg_);
+			map = job.default_generate_map(data_, island_off_center, labels_ptr, cfg_);
 			error_message = "";
 		}
 		catch (mapgen_exception& exc){
@@ -451,8 +415,8 @@ config default_map_generator::create_scenario(boost::optional<uint32_t> randomse
 			labels.begin(); i != labels.end(); ++i) {
 
 		if(i->first.x >= 0 && i->first.y >= 0 &&
-				i->first.x < static_cast<long>(width_) &&
-				i->first.y < static_cast<long>(height_)) {
+				i->first.x < static_cast<long>(data_.width) &&
+				i->first.y < static_cast<long>(data_.height)) {
 
 			config& label = res.add_child("label");
 			label["text"] = i->second;

src/generators/default_map_generator.hpp
@@ -18,6 +18,24 @@
 #include "config.hpp"
 #include "generators/map_generator.hpp"
 
+struct generator_data {
+	generator_data(const config& cfg);
+
+	int width;
+	int height;
+	int default_width;
+	int default_height;
+	int nplayers;
+	int nvillages;
+	int iterations;
+	int hill_size;
+	int castle_size;
+	int island_size;
+	int max_lakes;
+	bool link_castles;
+	bool show_labels;
+};
+
 class default_map_generator : public map_generator
 {
 public:
@@ -34,12 +52,11 @@ public:
 	config create_scenario(boost::optional<uint32_t> randomseed);
 
 private:
-
 	std::string generate_map(std::map<map_location,std::string>* labels, boost::optional<uint32_t> randomseed);
 
-	size_t default_width_, default_height_, width_, height_, island_size_, iterations_, hill_size_, max_lakes_, nvillages_, castle_size_, nplayers_;
-	bool link_castles_, show_labels_;
 	config cfg_;
+
+	generator_data data_;
 };
 
 #endif

commit 35dcb371cf508f2cbad784eb8c4d1aa356d0623a

Default Map Generator: some refactoring to fix issues with map generation introduced in ec7f8d7cf530 [formerly f52e9a96c5ae] [formerly f63439f467f2 [formerly e2c4310a5d12]] [formerly 3fbb3aeed677] [formerly 98bbc747c1c4] [formerly 6107a58f2c9a]
The cause was the data_ class member being modified every time the map was regenerated using itself
for the calculations, causing an eventually-fatal cumulative effect. This solves that by keeping a
temp copy of the data which is then modified and passed to the generator job.


Former-commit-id: bae03bef72dd61d3e3b35427db2d61fa3715cc4e
Former-commit-id: a951ed1edaddd20644b0d8959962062d68e136ae
Former-commit-id: 279e03f64d33e3701d6c29558a6e9be3e9f312db
Former-commit-id: d9a08329741462d113c242563ff6f16c159332c5 [formerly e65348d32d146fced4cb23e6142029becc8c8244]
Former-commit-id: 4c510acba3e87ef3e9dad659add0657437612a62

src/generators/default_map_generator.cpp
@@ -33,8 +33,8 @@ static lg::log_domain log_engine("engine");
 #define DBG_NG LOG_STREAM(debug, log_engine)
 
 namespace {
-	const size_t max_island = 10;
-	const size_t max_coastal = 5;
+	const int max_island = 10;
+	const int max_coastal = 5;
 }
 
 generator_data::generator_data(const config &cfg)
@@ -48,6 +48,7 @@ generator_data::generator_data(const config &cfg)
 	, hill_size(std::max(0, cfg["hill_size"].to_int(10)))
 	, castle_size(std::max(0, cfg["castle_size"].to_int(9)))
 	, island_size(std::max(0, cfg["island_size"].to_int(0)))
+	, island_off_center(0)
 	, max_lakes(std::max(0, cfg["max_lakes"].to_int(20)))
 	, link_castles(true)
 	, show_labels(true)
@@ -87,71 +88,85 @@ std::string default_map_generator::generate_map(std::map<map_location,std::strin
 	uint32_t seed;
 	if(const uint32_t* pseed = randomseed.get_ptr()) {
 		seed = *pseed;
-	}
-	else {
+	} else {
 		seed = seed_rng::next_seed();
 	}
 
+	/* We construct a copy of the generator data and modify it as needed. This ensures every time
+	 * this function is called the generator job gets a fresh set of settings, and that the internal
+	 * copy of the settings are never touched except by the settings dialog.
+	 *
+	 * The original data is still used for conditional checks and calculations, but any modifications
+	 * should be done on this object.
+	 */
+	generator_data job_data = data_;
+
 	// Suppress labels?
-	if ( !data_.show_labels )
+	if(!data_.show_labels) {
 		labels = nullptr;
+	}
 
-	// the random generator thinks odd widths are nasty, so make them even
-	if (is_odd(data_.width))
-		++data_.width;
+	// The random generator thinks odd widths are nasty, so make them even
+	if(is_odd(data_.width)) {
+		++job_data.width;
+	}
 
-	data_.iterations = (data_.iterations*data_.width*data_.height)/(data_.default_width*data_.default_height);
-	size_t island_off_center = 0;
+	job_data.iterations = (data_.iterations * data_.width * data_.height)/(data_.default_width * data_.default_height);
+	job_data.island_size = 0;
+	job_data.nvillages = (data_.nvillages * data_.width * data_.height) / 1000;
+	job_data.island_off_center = 0;
 
-	if(static_cast<unsigned>(data_.island_size) >= max_coastal) {
+	if(data_.island_size >= max_coastal) {
+		// Islands look good with much fewer iterations than normal, and fewer lakes
+		job_data.iterations /= 10;
+		job_data.max_lakes /= 9;
 
-		//islands look good with much fewer iterations than normal, and fewer lake
-		data_.iterations /= 10;
-		data_.max_lakes /= 9;
-
-		//the radius of the island should be up to half the width of the map
-		const size_t island_radius = 50 + ((max_island - data_.island_size)*50)/(max_island - max_coastal);
-		data_.island_size = (island_radius*(data_.width/2))/100;
+		// The radius of the island should be up to half the width of the map
+		const int island_radius = 50 + ((max_island - data_.island_size) * 50)/(max_island - max_coastal);
+		job_data.island_size = (island_radius * (data_.width/2))/100;
 	} else if(data_.island_size > 0) {
-		DBG_NG << "coastal...\n";
-		//the radius of the island should be up to twice the width of the map
-		const size_t island_radius = 40 + ((max_coastal - data_.island_size)*40)/max_coastal;
-		data_.island_size = (island_radius*data_.width*2)/100;
-		island_off_center = std::min<size_t>(data_.width,data_.height);
+		// The radius of the island should be up to twice the width of the map
+		const int island_radius = 40 + ((max_coastal - data_.island_size) * 40)/max_coastal;
+		job_data.island_size = (island_radius * data_.width * 2)/100;
+		job_data.island_off_center = std::min(data_.width, data_.height);
 		DBG_NG << "calculated coastal params...\n";
 	}
 
-	data_.nvillages = (data_.nvillages * data_.width * data_.height) / 1000;
-
 	// A map generator can fail so try a few times to get a map before aborting.
 	std::string map;
+
 	// Keep a copy of labels as it can be written to by the map generator func
 	std::map<map_location,std::string> labels_copy;
-	std::map<map_location,std::string> * labels_ptr =  labels ? &labels_copy : nullptr;
-	std::string error_message;
-	//initilize the job outside the loop so that we really get a different result everytime we run the loop.
+	std::map<map_location,std::string>* labels_ptr = labels ? &labels_copy : nullptr;
+
+	// Iinitilize the job outside the loop so that we really get a different result everytime we run the loop.
 	default_map_generator_job job(seed);
+
 	int tries = 10;
+	std::string error_message;
 	do {
-		if (labels) {
-			// Reset the labels.
+		// Reset the labels.
+		if(labels) {
 			labels_copy = *labels;
 		}
-		try{
-			map = job.default_generate_map(data_, island_off_center, labels_ptr, cfg_);
+
+		try {
+			map = job.default_generate_map(job_data, labels_ptr, cfg_);
 			error_message = "";
-		}
-		catch (mapgen_exception& exc){
+		} catch(mapgen_exception& exc) {
 			error_message = exc.message;
 		}
+
 		--tries;
-	} while (tries && map.empty());
-	if (labels) {
+	} while(tries && map.empty());
+
+	if(labels) {
 		labels->swap(labels_copy);
 	}
 
-	if (error_message != "")
+	if(!error_message.empty()) {
 		throw mapgen_exception(error_message);
+	}
 
 	return map;
 }

src/generators/default_map_generator.hpp
@@ -31,6 +31,7 @@ struct generator_data {
 	int hill_size;
 	int castle_size;
 	int island_size;
+	int island_off_center;
 	int max_lakes;
 	bool link_castles;
 	bool show_labels;

src/generators/default_map_generator_job.cpp
@@ -705,7 +705,7 @@ static void flood_name(const map_location& start, const std::string& name, std::
 	}
 }
 
-std::string default_map_generator_job::default_generate_map(generator_data data, size_t island_off_center, std::map<map_location,std::string>* labels, const config& cfg)
+std::string default_map_generator_job::default_generate_map(generator_data data, std::map<map_location,std::string>* labels, const config& cfg)
 {
 	log_scope("map generation");
 
@@ -759,7 +759,7 @@ std::string default_map_generator_job::default_generate_map(generator_data data,
 	}
 
 	// Generate the height of everything.
-	const height_map heights = generate_height_map(data.width, data.height, data.iterations, data.hill_size, data.island_size, island_off_center);
+	const height_map heights = generate_height_map(data.width, data.height, data.iterations, data.hill_size, data.island_size, data.island_off_center);
 
 	LOG_NG << "Done generating height map. " << (SDL_GetTicks() - ticks) << " ticks elapsed" << "\n";
 	ticks = SDL_GetTicks();

src/generators/default_map_generator_job.hpp
@@ -38,7 +38,7 @@ public:
 	default_map_generator_job(uint32_t seed);
 
 	/** Generate the map. */
-	std::string default_generate_map(generator_data data, size_t island_off_center, std::map<map_location,std::string>* labels, const config& cfg);
+	std::string default_generate_map(generator_data data, std::map<map_location,std::string>* labels, const config& cfg);
 
 private:
 	typedef std::vector<std::vector<int> > height_map;