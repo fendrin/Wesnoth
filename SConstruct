fendrin/Wesnoth

commit 869f34c917cc696f1d809fa0b4330e2dd14d7a63

scons: Don't pass a C++ compiler flag to C compilers
Needed since f9c0e4d33e80ce1200f4cf48c0d9e832469b38f7 [formerly e3122260a3af5baa963ad9cc4c3ed7372a015c99] [formerly 9649040fbe1edb9b114d281af923704e9a5886ee [formerly be29c021c9b501d20a4951a62627588c89e93d89]] [formerly a5ad2d226c5f6999c286af89496e4132c7463b21] [formerly 5e8e5c1e436ea661e7c98d7eb978c10bdf180cdb] [formerly acea95ff288e8980aee7c1f5d9d62a2b101ac524] introduces the
requirement of a C compiler in the building process. Otherwise we get
this from GCC 6.1.1:

  cc1: error: command line option ‘-Wold-style-cast’ is valid for C++/ObjC++ but not for C [-Werror]


Former-commit-id: 000a01dadd31edf8a8940c0ecad621e497f7a225
Former-commit-id: 69ba49f63a55f88c485ff916680ba518d131e0bd
Former-commit-id: 089d3040b2b48edea05b382779d777805b0dcb64
Former-commit-id: a5a793c3143e427be93138d7af1130e5fd1f8d5a [formerly 5d160c883f22ab1ed509111afe19b4d86a1976d7]
Former-commit-id: a8d8dea8a46985d74d6bcf21bfbddbe7f44e790e

SConstruct
@@ -512,7 +512,8 @@ for env in [test_env, campaignd_env, client_env, env]:
             env.AppendUnique(CXXFLAGS = ["-fopenmp"], LIBS = ["gomp"])
 
         if env['strict']:
-            env.AppendUnique(CCFLAGS = Split("-Werror -Wold-style-cast $(-Wno-unused-local-typedefs$)"))
+            env.AppendUnique(CCFLAGS = Split("-Werror $(-Wno-unsused-local-typedefs$)"))
+            env.AppendUnique(CXXFLAGS = Split("-Wold-style-cast"))
         if env['sanitize']:
             env.AppendUnique(CCFLAGS = ["-fsanitize=" + env["sanitize"]], LINKFLAGS = ["-fsanitize=" + env["sanitize"]])
 

commit e0952df41c4e6d1b31825830fc4a08884394ec94

scons: Fix compiler switch typo
Accidentally introduced in 869f34c917cc696f1d809fa0b4330e2dd14d7a63 [formerly a8d8dea8a46985d74d6bcf21bfbddbe7f44e790e] [formerly a5a793c3143e427be93138d7af1130e5fd1f8d5a [formerly 5d160c883f22ab1ed509111afe19b4d86a1976d7]] [formerly 089d3040b2b48edea05b382779d777805b0dcb64] [formerly 69ba49f63a55f88c485ff916680ba518d131e0bd] [formerly 000a01dadd31edf8a8940c0ecad621e497f7a225],
oops. Didn't become evident unless hitting other compiler errors,
though.


Former-commit-id: 971e6c448c7b6f7472b86bea393f59da6a4caf1c
Former-commit-id: c60c407245912bc0d607db76f36007983877f1a5
Former-commit-id: 53af19f04793b443b278615d23dfd75f227cd72d
Former-commit-id: 4af236629b289a1ada4a4be0221fb9dd02f83ab2 [formerly 0cc9696b7a73a453db8cdf48749ac4a895801072]
Former-commit-id: e0aaa654779c5ad8bcfa57b2c06a96ca24160fa9

SConstruct
@@ -512,7 +512,7 @@ for env in [test_env, campaignd_env, client_env, env]:
             env.AppendUnique(CXXFLAGS = ["-fopenmp"], LIBS = ["gomp"])
 
         if env['strict']:
-            env.AppendUnique(CCFLAGS = Split("-Werror $(-Wno-unsused-local-typedefs$)"))
+            env.AppendUnique(CCFLAGS = Split("-Werror $(-Wno-unused-local-typedefs$)"))
             env.AppendUnique(CXXFLAGS = Split("-Wold-style-cast"))
         if env['sanitize']:
             env.AppendUnique(CCFLAGS = ["-fsanitize=" + env["sanitize"]], LINKFLAGS = ["-fsanitize=" + env["sanitize"]])