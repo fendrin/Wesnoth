fendrin/Wesnoth

commit 0196e1a3a35f185b833725f3eec2ff3d43da3500

Cleaned up handling of basic alpha setting
This does a few things:
* Refactored adjust_surface_alpha to use SDL_SetSurfaceAlphaMod
* Made all uses of the legacy SDL_SetAlpha use adjust_surface_alpha
* Made adjust_surface_alpha now take a non-const reference and perform the operation on the surface directly.


Former-commit-id: 552b7ff3c289e3a7b89cf905f76c003e98563a9a
Former-commit-id: 6b65e1f3782829faaf0134280150fb1baeec275f
Former-commit-id: 2f266b6801d027cb08b85e36cd7c0fea4baff9fe
Former-commit-id: 76ae9694177cced18e3da7c9457d2f0fd1001cde [formerly 4bd60a3f603eb14efa9b25dc007e8a7f8cdd1643]
Former-commit-id: 95e14f53b64102f743366633152ecc25257f271b

src/display.cpp
@@ -1758,7 +1758,7 @@ void display::render_image(int x, int y, const display::tdrawing_layer drawing_l
 	//} else if(alpha != 1.0 && blendto != 0) {
 	//	surf.assign(blend_surface(surf,1.0-alpha,blendto));
 	} else if(alpha != ftofxp(1.0)) {
-		surf = adjust_surface_alpha(surf, alpha, false);
+		adjust_surface_alpha(surf, alpha);
 	}
 
 	if(surf == nullptr) {

src/CMakeLists.txt
@@ -389,7 +389,6 @@ set_target_properties(wesnoth-lua
 ########### Helper libraries ###############
 
 set(wesnoth-sdl_SRC
-	sdl/alpha.cpp
 	sdl/exception.cpp
 	sdl/rect.cpp
 	sdl/image.cpp

src/SConscript
@@ -158,7 +158,6 @@ libcampaignd_sources = Split("""
 libcampaignd = env.Library("campaignd", libcampaignd_sources, OBJPREFIX = "campaignd_")
 
 libwesnoth_sdl_sources = Split("""
-    sdl/alpha.cpp
     sdl/exception.cpp
     sdl/image.cpp
     sdl/rect.cpp
@@ -690,7 +689,6 @@ test_utils_sources = Split("""
     """)
 
 wesmage_sources = Split("""
-    sdl/alpha.cpp
     sdl/utils.cpp
     sdl/window.cpp
     tools/dummy_video.cpp
@@ -742,7 +740,6 @@ test_sources.extend(test_env.Object("tests/test_config_cache.cpp"))
 test = test_env.WesnothProgram("test", test_sources +  [libtest_utils, libwesnoth_extras, libwesnoth_core, libwesnoth, libwesnoth_sdl, libwesnoth_extras], have_test_prereqs)
 
 create_images_sources = Split("""
-    sdl/alpha.cpp
     sdl/utils.cpp
     sdl/window.cpp
     tests/create_images.cpp

src/attack_prediction_display.cpp
@@ -23,7 +23,6 @@
 #include "language.hpp"
 #include "marked-up_text.hpp"
 #include "resources.hpp"
-#include "sdl/alpha.hpp"
 #include "units/unit.hpp"
 #include "units/abilities.hpp"
 
@@ -431,7 +430,7 @@ void battle_prediction_pane::get_hp_distrib_surface(const std::vector<std::pair<
 	surf = create_neutral_surface(width, height);
 
 	// Disable alpha channel to avoid problem with sdl_blit
-	SDL_SetAlpha(surf, 0, SDL_ALPHA_OPAQUE);
+	adjust_surface_alpha(surf, SDL_ALPHA_OPAQUE);
 
 	SDL_Rect clip_rect = sdl::create_rect(0, 0, width, height);
 	Uint32 grey_color = SDL_MapRGBA(surf->format, 0xb7, 0xc1, 0xc1, SDL_ALPHA_OPAQUE);

src/editor/action/mouse/mouse_action.cpp
@@ -183,7 +183,7 @@ void mouse_action::set_terrain_mouse_overlay(editor_display& disp, const t_trans
 	image = mask_surface(image, image::get_hexmask());
 
 	// Add the alpha factor
-	image = adjust_surface_alpha(image, alpha);
+	adjust_surface_alpha(image, alpha);
 
 	// scale the image
 	const int zoom = disp.hex_size();
@@ -344,7 +344,8 @@ void mouse_action_paste::set_mouse_overlay(editor_display& disp)
 	int zoom = static_cast<int>(size * disp.get_zoom_factor());
 
 	// Add the alpha factor and scale the image
-	image = scale_surface(adjust_surface_alpha(image, alpha), zoom, zoom);
+	adjust_surface_alpha(image, alpha);
+	image = scale_surface(image, zoom, zoom);
 	disp.set_mouseover_hex_overlay(image);
 }
 
@@ -399,7 +400,7 @@ editor_action* mouse_action_starting_position::up_left(editor_display& disp, int
 		return nullptr;
 	}
 	auto player_starting_at_hex = disp.map().is_starting_position(hex);
-	 
+
 	if (has_ctrl_modifier()) {
 		if (player_starting_at_hex) {
 			location_palette_.add_item(*player_starting_at_hex);
@@ -461,7 +462,8 @@ void mouse_action_starting_position::set_mouse_overlay(editor_display& disp)
 	int zoom = static_cast<int>(size * disp.get_zoom_factor());
 
 	// Add the alpha factor and scale the image
-	image = scale_surface(adjust_surface_alpha(image, alpha), zoom, zoom);
+	adjust_surface_alpha(image, alpha);
+	image = scale_surface(image, zoom, zoom);
 	disp.set_mouseover_hex_overlay(image);
 }
 

src/editor/action/mouse/mouse_action_item.cpp
@@ -244,7 +244,8 @@ void mouse_action_item::set_item_mouse_overlay(editor_display& disp, const overl
 	int zoom = static_cast<int>(size * disp.get_zoom_factor());
 
 	// Add the alpha factor and scale the image
-	image = scale_surface(adjust_surface_alpha(image, alpha), zoom, zoom);
+	adjust_surface_alpha(image, alpha);
+	image = scale_surface(image, zoom, zoom);
 	disp.set_mouseover_hex_overlay(image);
 }
 

src/editor/action/mouse/mouse_action_map_label.cpp
@@ -121,7 +121,8 @@ void mouse_action_map_label::set_mouse_overlay(editor_display& disp)
 	int zoom = static_cast<int>(size * disp.get_zoom_factor());
 
 	// Add the alpha factor and scale the image
-	image = scale_surface(adjust_surface_alpha(image, alpha), zoom, zoom);
+	adjust_surface_alpha(image, alpha);
+	image = scale_surface(image, zoom, zoom);
 	disp.set_mouseover_hex_overlay(image);
 }
 

src/editor/action/mouse/mouse_action_select.cpp
@@ -70,7 +70,8 @@ void mouse_action_select::set_mouse_overlay(editor_display& disp)
 	int zoom = static_cast<int>(size * disp.get_zoom_factor());
 
 	// Add the alpha factor and scale the image
-	image = scale_surface(adjust_surface_alpha(image, alpha), zoom, zoom);
+	adjust_surface_alpha(image, alpha);
+	image = scale_surface(image, zoom, zoom);
 	disp.set_mouseover_hex_overlay(image);
 }
 

commit 8398f49ac36f2ec37d28c0cb327416f5d79a7908

Attempt to fix recent unit drawing problems introduced in 0196e1a3a35 [formerly 95e14f53b64] [formerly 76ae9694177 [formerly 4bd60a3f603]] [formerly 2f266b6801d] [formerly 6b65e1f3782] [formerly 552b7ff3c28]
Former-commit-id: 8ce2f817bfcc21ae245ef80279a9f3de916921e4
Former-commit-id: d671b9e101e297bdfba929ad054ab498f70a37de
Former-commit-id: 742a5fbea87f76e7baadb4ea7c3038de043560d5
Former-commit-id: 98f839d248bcde91d174bd66f7936407d6efc09b [formerly 4f92e3fb7a4636b803ef8e2001c482063e7d62c4]
Former-commit-id: 370a7d12befdd9c7e7fcf5732d0da2bf16406587

src/display.cpp
@@ -1758,7 +1758,9 @@ void display::render_image(int x, int y, const display::tdrawing_layer drawing_l
 	//} else if(alpha != 1.0 && blendto != 0) {
 	//	surf.assign(blend_surface(surf,1.0-alpha,blendto));
 	} else if(alpha != ftofxp(1.0)) {
-		adjust_surface_alpha(surf, alpha);
+		surface temp = make_neutral_surface(surf);
+		adjust_surface_alpha(temp, alpha);
+		surf = temp;
 	}
 
 	if(surf == nullptr) {