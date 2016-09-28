fendrin/Wesnoth

commit c8462d073d43d2718483d5b34ea0e3d02611db24

Change most vector<bool> to dynamic_bitset
A few cases were skipped, most of which involved 2D bitsets (eg shroud)
or uses that could not be easily translated (eg GUI1 multimenu).

In a few cases, code needed to be updated for the different API that the
dynamic_bitset provides, but in most cases, this change is entirely
transparent.

There are probably a few unnecessary dynamic_bitset.hpp includes.

One use of vector<bool> was replaced instead with std::bitset, since
the dynamic size was not required.


Former-commit-id: 3dc09393358e27bc7191850e0c2fe5d6469f3bfa
Former-commit-id: 56c5c59c78e1bf2a560f3d66ba172660b7d91760
Former-commit-id: 6374efdf31196be1a688edfae3203f2b6794f69c
Former-commit-id: 2d751c6dc6b2d2be6a1d7197e90b15a32acd59b8 [formerly b731f61abbddd3814bdd61b8eac10d92ca0f595d]
Former-commit-id: 568d968f82f5c4eb4a5ad5aa2dc03f7f42259710

src/addon/manager_ui.cpp
@@ -42,6 +42,8 @@
 
 #include "addon/client.hpp"
 
+#include <boost/dynamic_bitset.hpp>
+
 static lg::log_domain log_config("config");
 static lg::log_domain log_network("network");
 static lg::log_domain log_filesystem("filesystem");
@@ -447,7 +449,7 @@ public:
 struct addons_filter_state
 {
 	std::string keywords;
-	std::vector<bool> types;
+	boost::dynamic_bitset<> types;
 	ADDON_STATUS_FILTER status;
 	// Yes, the sorting criterion and direction are part of the
 	// filter options since changing them requires rebuilding the
@@ -489,14 +491,14 @@ public:
 
 		dlg.show(video_);
 
-		const std::vector<bool> new_types = dlg.displayed_types();
+		const boost::dynamic_bitset<> new_types = dlg.displayed_types();
 		const ADDON_STATUS_FILTER new_status = dlg.displayed_status();
 		const ADDON_SORT new_sort = dlg.sort();
 		const ADDON_SORT_DIRECTION new_direction = dlg.direction();
 
 		assert(f_.types.size() == new_types.size());
 
-		if(std::equal(f_.types.begin(), f_.types.end(), new_types.begin()) && f_.status == new_status &&
+		if(f_.types == new_types && f_.status == new_status &&
 		   f_.sort == new_sort && f_.direction == new_direction) {
 			// Close the manager dialog only if the filter options changed.
 			return gui::CONTINUE_DIALOG;

src/actions/vision.cpp
@@ -35,6 +35,8 @@
 #include "team.hpp"
 #include "units/unit.hpp"
 
+#include <boost/dynamic_bitset.hpp>
+
 class unit_animation;
 
 static lg::log_domain log_engine("engine");
@@ -636,7 +638,8 @@ bool actor_sighted(const unit & target, const std::vector<int> * cache)
 	const map_location & target_loc = target.get_location();
 
 	// Determine the teams that (probably) should get events.
-	std::vector<bool> needs_event(teams_size, cache == nullptr);
+	boost::dynamic_bitset<> needs_event;
+	needs_event.resize(teams_size, cache == nullptr);
 	if ( cache != nullptr ) {
 		// Flag just the sides in the cache as needing events.
 		for (int side : *cache)

src/ai/default/ca.cpp
@@ -35,6 +35,7 @@
 #include "pathfind/teleport.hpp"
 
 #include <numeric>
+#include <boost/dynamic_bitset.hpp>
 
 #include <SDL.h>
 
@@ -983,8 +984,7 @@ void get_villages_phase::dispatch_complex(
 	std::multimap<size_t /* villages_per_unit value*/,
 		size_t /*villages_per_unit index*/> unit_lookup;
 
-	std::vector</*unit*/std::vector</*village*/bool> >
-		matrix(reachmap.size(), std::vector<bool>(village_count, false));
+	std::vector</*unit*/boost::dynamic_bitset</*village*/>> matrix(reachmap.size(), boost::dynamic_bitset<>(village_count));
 
 	treachmap::const_iterator itor = reachmap.begin();
 	for(size_t u = 0; u < unit_count; ++u, ++itor) {
@@ -1072,28 +1072,22 @@ void get_villages_phase::dispatch_complex(
 				continue;
 			}
 
-			std::vector<bool> result;
-			std::transform(matrix[src_itor->second].begin(), matrix[src_itor->second].end(),
-				matrix[dst_itor->second].begin(),
-				std::back_inserter(result),
-				std::logical_and<bool>()
-				);
-
-			size_t matched = std::count(result.begin(), result.end(), true);
+			boost::dynamic_bitset<> result = matrix[src_itor->second] & matrix[dst_itor->second];
+			size_t matched = result.count();
 
 			// we found a  solution, dispatch
 			if(matched == 2) {
 				// Collect data
-				std::vector<bool>::iterator first = std::find(result.begin(), result.end(), true);
-				std::vector<bool>::iterator second = std::find(first + 1, result.end(), true);
+				size_t first = result.find_first();
+				size_t second = result.find_next(first);
 
-				const map_location village1 = villages[first - result.begin()];
-				const map_location village2 = villages[second - result.begin()];
+				const map_location village1 = villages[first];
+				const map_location village2 = villages[second];
 
 				const bool perfect = (src_itor->first == 2 &&
 					dst_itor->first == 2 &&
-					units_per_village[first - result.begin()] == 2 &&
-					units_per_village[second - result.begin()] == 2);
+					units_per_village[first] == 2 &&
+					units_per_village[second] == 2);
 
 				// Dispatch
 				DBG_AI_TESTING_AI_DEFAULT << "Found a square.\nDispatched unit at " << units[src_itor->second]

src/game_initialization/multiplayer_lobby.hpp
@@ -22,6 +22,8 @@
 #include "image.hpp"
 #include "serialization/string_utils.hpp"
 
+#include <boost/dynamic_bitset.hpp>
+
 class config;
 class video;
 class CVideo;
@@ -212,8 +214,8 @@ private:
 		lobby_sorter(const config& cfg);
 	};
 
-	std::vector<bool> game_vacant_slots_;
-	std::vector<bool> game_observers_;
+	boost::dynamic_bitset<> game_vacant_slots_;
+	boost::dynamic_bitset<> game_observers_;
 
 	gui::button observe_game_;
 	gui::button join_game_;

src/gui/dialogs/addon/filter_options.cpp
@@ -30,6 +30,8 @@
 
 #include "utils/functional.hpp"
 
+#include <boost/dynamic_bitset.hpp>
+
 namespace
 {
 bool unchecked_bool_field_finder(gui2::twindow& window,
@@ -150,7 +152,7 @@ void taddon_filter_options::register_displayed_type_field(
 			register_bool(field_id, true, displayed_types_[addon_type]));
 }
 
-void taddon_filter_options::read_types_vector(const std::vector<bool>& v)
+void taddon_filter_options::read_types_vector(const boost::dynamic_bitset<>& v)
 {
 	for(size_t k = 0; k < displayed_types_.size(); ++k) {
 		// All unspecified types default to visible.

src/gui/dialogs/addon/filter_options.hpp
@@ -22,6 +22,7 @@
 #include "addon/state.hpp"
 
 #include <array>
+#include <boost/dynamic_bitset.hpp>
 
 namespace gui2
 {
@@ -33,13 +34,12 @@ class taddon_filter_options : public tdialog
 public:
 	taddon_filter_options();
 
-	std::vector<bool> displayed_types() const
+	boost::dynamic_bitset<> displayed_types() const
 	{
-		return std::vector<bool>(displayed_types_.begin(),
-								 displayed_types_.end());
+		return boost::dynamic_bitset<>(displayed_types_.begin(), displayed_types_.end());
 	}
 
-	void set_displayed_types(const std::vector<bool>& types)
+	void set_displayed_types(const boost::dynamic_bitset<>& types)
 	{
 		read_types_vector(types);
 	}
@@ -92,7 +92,7 @@ private:
 	void register_displayed_type_field(const std::string& field_id,
 									   ADDON_TYPE addon_type);
 
-	void read_types_vector(const std::vector<bool>& v);
+	void read_types_vector(const boost::dynamic_bitset<>& v);
 
 	void toggle_all_displayed_types_button_callback(twindow& window);
 

src/gui/dialogs/addon/list.cpp
@@ -173,8 +173,7 @@ void taddon_list::on_filtertext_changed(ttext_* textbox, const std::string& text
 {
 	tlistbox& listbox = find_widget<tlistbox>(textbox->get_window(), "addons", true);
 	filter_transform filter(utils::split(text, ' '));
-	std::vector<bool> res;
-	res.reserve(cfg_.child_count("campaign"));
+	boost::dynamic_bitset<> res;
 	for(const auto& child : cfg_.child_range("campaign"))
 	{
 		res.push_back(filter(child));

src/gui/dialogs/game_load.cpp
@@ -234,7 +234,7 @@ void tgame_load::filter_text_changed(ttext_* textbox, const std::string& text)
 		return;
 	last_words_ = words;
 
-	std::vector<bool> show_items(list.get_item_count(), true);
+	boost::dynamic_bitset<> show_items(list.get_item_count(), true);
 
 	if(!text.empty()) {
 		for(unsigned int i = 0; i < list.get_item_count(); i++) {

commit e6d4805bf0dc28d5c0e9da2fc22a0749312257ee

addon/mg: Fix regression causing all categories to be filtered out by default
Turns out unsigned long(true) is not a comprehensive enough value for
setting a boost::dynamic_bitset to have all bits initially on.
Regression introduced by commit
c8462d073d43d2718483d5b34ea0e3d02611db24 [formerly 568d968f82f5c4eb4a5ad5aa2dc03f7f42259710] [formerly 2d751c6dc6b2d2be6a1d7197e90b15a32acd59b8 [formerly b731f61abbddd3814bdd61b8eac10d92ca0f595d]] [formerly 6374efdf31196be1a688edfae3203f2b6794f69c] [formerly 56c5c59c78e1bf2a560f3d66ba172660b7d91760] [formerly 3dc09393358e27bc7191850e0c2fe5d6469f3bfa].

Fixes the more visible half of bug #25073.


Former-commit-id: e38e9ceb5aabbc64afc5c670e677f233cf3f50b2
Former-commit-id: 2f1293264125cfa6b0e9ee2d865f62d7f7ec52db
Former-commit-id: 3598ec7fbbf4f14c15ae70f8669e2192f3deb3c7
Former-commit-id: b0e12a3604bc33adc7824b21658e5c31840791ce [formerly aecd6dac375915f89ace3c048d1ef4929d126c9b]
Former-commit-id: dcd5a794ce0d39964f9feefadc058f95f008014d

src/addon/manager_ui.cpp
@@ -460,7 +460,7 @@ struct addons_filter_state
 
 	addons_filter_state()
 		: keywords()
-		, types(ADDON_TYPES_COUNT, true)
+		, types(ADDON_TYPES_COUNT, 0xFFFFFFFFUL)
 		, status(FILTER_ALL)
 		, sort(SORT_NAMES)
 		, direction(DIRECTION_ASCENDING)