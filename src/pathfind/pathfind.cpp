fendrin/Wesnoth

commit 7169d18c060ff25e784ec085470e1abadea94753

Tunnels: fix vision not being propagated through tunnels
Former-commit-id: 7fe5242966f351c9c0af1ad9dbad2e14c98d3235
Former-commit-id: 2fb86e6c8215bc7b8599653c8a1f9483f0089176
Former-commit-id: f12fb8c54718c3615418615fae8471233647d05c
Former-commit-id: fcfee8cf56dddf6c62c1e4d0a663331d06807425 [formerly 52dd452fe3b7ae131a79a728cbf404bacbf1eba7]
Former-commit-id: df19cf7c563eb4b93fcc16e04ff04fc5278eb8ea

src/pathfind/pathfind.cpp
@@ -242,7 +242,7 @@ namespace {
  *                           destinations. (It is permissible for this to contain
  *                           some hexes that are also in destinations.)
  *
- * @param[in]  teleporter    If not nullptr, teleportaion will be considered, using
+ * @param[in]  teleporter    If not nullptr, teleportation will be considered, using
  *                           this unit's abilities.
  * @param[in]  current_team  If not nullptr, enemies of this team can obstruct routes
  *                           both by occupying hexes and by exerting zones of control.
@@ -263,6 +263,8 @@ namespace {
  *                           Destinations is ignored.
  *                           full_cost_map is a vector of pairs. The first entry is the
  *                           cost itself, the second how many units already visited this hex
+ * @param[in]  check_vision  If true, use vision check for teleports, that is, ignore
+ *                           units potentially blocking the teleport exit
  */
 static void find_routes(
 		const map_location & origin, const movetype::terrain_costs & costs,
@@ -271,7 +273,7 @@ static void find_routes(
 		const unit * teleporter, const team * current_team,
 		const unit * skirmisher, const team * viewing_team,
 		const std::map<map_location, int> * jamming_map=nullptr,
-		std::vector<std::pair<int, int> > * full_cost_map=nullptr)
+		std::vector<std::pair<int, int> > * full_cost_map=nullptr, bool check_vision=false)
 {
 	const gamemap& map = resources::gameboard->map();
 
@@ -283,7 +285,7 @@ static void find_routes(
 
 	// Build a teleport map, if needed.
 	const teleport_map teleports = teleporter ?
-			get_teleport_locations(*teleporter, *viewing_team, see_all, current_team == nullptr) :
+			get_teleport_locations(*teleporter, *viewing_team, see_all, current_team == nullptr, check_vision) :
 			teleport_map();
 
 	// Since this is called so often, keep memory reserved for the node list.
@@ -558,11 +560,12 @@ vision_path::vision_path(const unit& viewer, map_location const &loc,
 {
 	const int sight_range = viewer.vision();
 
-	// The four nullptr parameters indicate (in order): no teleports,
-	// ignore units, ignore ZoC (no effect), and see all (no effect).
+	// The three nullptr parameters indicate (in order):
+	// ignore units, ignore ZoC (no effect), and don't build a cost_map.
+	team const& viewing_team = resources::gameboard->teams()[resources::screen->viewing_team()];
 	find_routes(loc, viewer.movement_type().get_vision(),
 	            viewer.get_state(unit::STATE_SLOWED), sight_range, sight_range,
-	            0, destinations, &edges, nullptr, nullptr, nullptr, nullptr, &jamming_map);
+	            0, destinations, &edges, &viewer, nullptr, nullptr, &viewing_team, &jamming_map, nullptr, true);
 }
 
 /**
@@ -581,10 +584,12 @@ vision_path::vision_path(const movetype::terrain_costs & view_costs, bool slowed
                          const std::map<map_location, int>& jamming_map)
 	: paths(), edges()
 {
-	// The four nullptr parameters indicate (in order): no teleports,
-	// ignore units, ignore ZoC (no effect), and see all (no effect).
+	// The three nullptr parameters indicate (in order):
+	// ignore units, ignore ZoC (no effect), and don't build a cost_map.
+	team const& viewing_team = resources::gameboard->teams()[resources::screen->viewing_team()];
+	const unit_map::const_iterator u = resources::units->find(loc);
 	find_routes(loc, view_costs, slowed, sight_range, sight_range, 0,
-	            destinations, &edges, nullptr, nullptr, nullptr, nullptr, &jamming_map);
+	            destinations, &edges, &*u, nullptr, nullptr, &viewing_team, &jamming_map, nullptr, true);
 }
 
 /// Default destructor

src/pathfind/teleport.cpp
@@ -163,7 +163,8 @@ teleport_map::teleport_map(
 		, const unit& u
 		, const team &viewing_team
 		, const bool see_all
-		, const bool ignore_units)
+		, const bool ignore_units
+		, const bool check_vision)
 	: teleport_map_()
 	, sources_()
 	, targets_()
@@ -187,7 +188,7 @@ teleport_map::teleport_map(
 			locations.second.swap(filter_locs.second);
 		}
 
-		if (!group.pass_allied_units() && !ignore_units) {
+		if (!group.pass_allied_units() && !ignore_units && !check_vision) {
 			std::set<map_location>::iterator loc = locations.second.begin();
 			while(loc != locations.second.end()) {
 				const unit *v = resources::gameboard->get_visible_unit(*loc, viewing_team, see_all);
@@ -248,7 +249,7 @@ void teleport_map::get_targets(std::set<map_location>& targets) const {
 
 const teleport_map get_teleport_locations(const unit &u,
 	const team &viewing_team,
-	bool see_all, bool ignore_units)
+	bool see_all, bool ignore_units, bool check_vision)
 {
 	std::vector<teleport_group> groups;
 
@@ -265,7 +266,7 @@ const teleport_map get_teleport_locations(const unit &u,
 	const std::vector<teleport_group>& global_groups = resources::tunnels->get();
 	groups.insert(groups.end(), global_groups.begin(), global_groups.end());
 
-	return teleport_map(groups, u, viewing_team, see_all, ignore_units);
+	return teleport_map(groups, u, viewing_team, see_all, ignore_units, check_vision);
 }
 
 manager::manager(const config &cfg) : tunnels_(), id_(cfg["next_teleport_group_id"].to_int(0)) {

src/pathfind/teleport.hpp
@@ -94,13 +94,15 @@ public:
 	 * @param viewing_team
 	 * @param see_all
 	 * @param ignore_units
+	 * @param check_vision
 	 */
 	teleport_map(
 			  const std::vector<teleport_group>& teleport_groups
 			, const unit& u
 			, const team &viewing_team
 			, const bool see_all
-			, const bool ignore_units);
+			, const bool ignore_units
+			, const bool check_vision);
 
 	/*
 	 * Constructs an empty teleport map.
@@ -140,10 +142,11 @@ private:
  * @param viewing_team		The team the player belongs to
  * @param see_all			Whether the teleport can be seen below shroud
  * @param ignore_units		Whether to ignore zoc and blocking by units
+ * @param check_vision		Whether to check vision as opposed to movement range
  * @returns a teleport_map
  */
 const teleport_map get_teleport_locations(const unit &u, const team &viewing_team,
-		bool see_all = false, bool ignore_units = false);
+		bool see_all = false, bool ignore_units = false, bool check_vision = false);
 
 class manager
 {

commit 59821902a8d4450cba6d4c49828bb98775fcda51

fix assertion failure when shroud/FoW update undelayed
https://gna.org/bugs/?25106

was broken in https://github.com/wesnoth/wesnoth/commit/7169d18c060ff25e784ec085470e1abadea94753 [formerly df19cf7c563eb4b93fcc16e04ff04fc5278eb8ea] [formerly fcfee8cf56dddf6c62c1e4d0a663331d06807425 [formerly 52dd452fe3b7ae131a79a728cbf404bacbf1eba7]] [formerly f12fb8c54718c3615418615fae8471233647d05c] [formerly 2fb86e6c8215bc7b8599653c8a1f9483f0089176] [formerly 7fe5242966f351c9c0af1ad9dbad2e14c98d3235]
Former-commit-id: c7e573d5c04c398cbc5670bd73201e9fd80116b2
Former-commit-id: 385828baf8b2e3b29fd02e791e656d6d7c80dd5c
Former-commit-id: 9f4427b0277eb1e07b61c3c04a2225d9aedb2739
Former-commit-id: d83662a13f01b93582243134951a77e513f9f30c [formerly 18c8ddf32c49e47a801555c491cda1c77664647d]
Former-commit-id: 2ba87f0c5aec20213ccf511f5f0c6567a80b03fb

src/pathfind/pathfind.cpp
@@ -589,7 +589,7 @@ vision_path::vision_path(const movetype::terrain_costs & view_costs, bool slowed
 	team const& viewing_team = resources::gameboard->teams()[resources::screen->viewing_team()];
 	const unit_map::const_iterator u = resources::units->find(loc);
 	find_routes(loc, view_costs, slowed, sight_range, sight_range, 0,
-	            destinations, &edges, &*u, nullptr, nullptr, &viewing_team, &jamming_map, nullptr, true);
+	            destinations, &edges, u.valid() ? &*u : nullptr, nullptr, nullptr, &viewing_team, &jamming_map, nullptr, true);
 }
 
 /// Default destructor