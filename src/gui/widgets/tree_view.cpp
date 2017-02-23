fendrin/Wesnoth

commit bf36d3f0677fe3d6213ba617d9556b8b0e1e9d0e

Tree View: cleaned up remove_node and made it return the removed node's position
Former-commit-id: cee0612f0c0e229494d942a7eca44dacf60a574a
Former-commit-id: bab30e454ad6d48c5fa3102326545b4cd623a623
Former-commit-id: 9849dd6288c1854f510075eac45c2e120ae1c9f6
Former-commit-id: cb1788ac75baa7a92924f1a21e6e79478a614dfe [formerly 407d67873a69fba81076ef4c5c333a41f02b6871]
Former-commit-id: 689ec992b4ac4920c508ae7ab7680c485619bf74

src/gui/widgets/tree_view.cpp
@@ -67,30 +67,23 @@ ttree_view_node& ttree_view::add_node(
 	return get_root_node().add_child(id, data);
 }
 
-void ttree_view::remove_node(ttree_view_node* node)
+int ttree_view::remove_node(ttree_view_node* node)
 {
 	assert(node && node != root_node_ && node->parent_node_);
-	const tpoint node_size = node->get_size();
 
-	boost::ptr_vector<ttree_view_node>::iterator itor
-			= node->parent_node_->children_.begin();
+	ttree_view_node::node_children_vector& parent_children = node->parent_node_->children_;
 
-	for(; itor != node->parent_node_->children_.end(); ++itor) {
-		if(&*itor == node) {
-			break;
-		}
+	auto itor = std::find(parent_children.begin(), parent_children.end(), *node);
+	assert(itor != parent_children.end());
+
+	parent_children.erase(itor);
+
+	if(get_size() != tpoint()) {
+		// Don't shrink the width, need to think about a good algorithm to do so.
+		resize_content(0, -node->get_size().y);
 	}
 
-	assert(itor != node->parent_node_->children_.end());
-
-	node->parent_node_->children_.erase(itor);
-
-	if(get_size() == tpoint()) {
-		return;
-	}
-
-	// Don't shrink the width, need to think about a good algorithm to do so.
-	resize_content(0, -node_size.y);
+	return itor - parent_children.begin();
 }
 
 void ttree_view::clear()

src/gui/widgets/tree_view.hpp
@@ -59,7 +59,7 @@ public:
 	add_node(const std::string& id,
 			 const std::map<std::string /* widget id */, string_map>& data);
 
-	void remove_node(ttree_view_node* tree_view_node);
+	int remove_node(ttree_view_node* tree_view_node);
 
 	void clear();
 

commit 44e470274d2e3c38ce0ba298f75f0a9775d3067b

Fix UB in tree_view::remove_node()
The function used a node object after deleting it and an iterator after
invalidating it. Both are undefined behavior.

Thanks to @gfgtdf for spotting the first bug.

Both bugs were introduced in commit bf36d3f0677fe3d6213ba617d9556b8b0e1e9d0e [formerly 689ec992b4ac4920c508ae7ab7680c485619bf74] [formerly cb1788ac75baa7a92924f1a21e6e79478a614dfe [formerly 407d67873a69fba81076ef4c5c333a41f02b6871]] [formerly 9849dd6288c1854f510075eac45c2e120ae1c9f6] [formerly bab30e454ad6d48c5fa3102326545b4cd623a623] [formerly cee0612f0c0e229494d942a7eca44dacf60a574a].


Former-commit-id: b31326ae4fbde8d378a210c0ba206d06b99b2684
Former-commit-id: aca3dc8af79f232155c4cf2675b62a5f25234af5
Former-commit-id: d6c45fabe7ee66c70500563146580a4cdc076836
Former-commit-id: bcf6876f4e2543671f43591362cce5f2cad33803 [formerly 40e4d38bc0859203f2e17e2c8c42ac39dda07fcd]
Former-commit-id: 2a566f88a52765e687204c0379804d0c5e26659b

src/gui/widgets/tree_view.cpp
@@ -71,20 +71,23 @@ tree_view_node& tree_view::add_node(
 int tree_view::remove_node(tree_view_node* node)
 {
 	assert(node && node != root_node_ && node->parent_node_);
+	const point node_size = node->get_size();
 
 	tree_view_node::node_children_vector& siblings = node->parent_node_->children_;
 
-	auto itor = std::find(siblings.begin(), siblings.end(), *node);
-	assert(itor != siblings.end());
+	auto node_itor = std::find(siblings.begin(), siblings.end(), *node);
+	assert(node_itor != siblings.end());
 
-	siblings.erase(itor);
+	const int position = node_itor - siblings.begin();
+
+	siblings.erase(node_itor);
 
 	if(get_size() != point()) {
 		// Don't shrink the width, need to think about a good algorithm to do so.
-		resize_content(0, -node->get_size().y);
+		resize_content(0, -node_size.y);
 	}
 
-	return itor - siblings.begin();
+	return position;
 }
 
 void tree_view::clear()