fendrin/Wesnoth

commit abc859612d12ae36142905f6c6eac081ab5ae155

GUI2: When a label grows, attempt to only lay out the containing grid
This greatly improves responsiveness of the add-on manager (bug #25523).


Former-commit-id: 03bb137d9c8219f50515619d3f42c3de11a05e0c
Former-commit-id: 1b663fc184eb1f7a16dd8a5dad90acf6c38461f7
Former-commit-id: 46b89eee3118e3e3c2a4f59757ffd4ef25ce427b
Former-commit-id: 31aff52bb1a545c7475c4978ce660e202143ff57 [formerly cf643e2bbfad22c46975f75415c1c2e9cd017929]
Former-commit-id: e0eb41027d3acc08bdacbb727d2cf96f5fca7d5a

src/gui/widgets/grid.cpp
@@ -20,6 +20,7 @@
 #include "gui/core/log.hpp"
 #include "gui/core/layout_exception.hpp"
 #include "gui/widgets/styled_widget.hpp"
+#include "gui/widgets/window.hpp"
 
 #include <numeric>
 
@@ -375,6 +376,43 @@ void grid::request_reduce_height(const unsigned maximum_height)
 void grid::demand_reduce_height(const unsigned /*maximum_height*/)
 {
 	/** @todo Implement. */
+}
+
+void grid::relayout()
+{
+	point size = get_size();
+	point best_size = calculate_best_size();
+	if(size.x >= best_size.x && size.y >= best_size.y) {
+		place(get_origin(), size);
+		return;
+	}
+
+	recalculate_best_size();
+
+	if(size.y >= best_size.y) {
+		// We have enough space in the Y direction, but not in the X direction.
+		// Try wrapping the content.
+		request_reduce_width(size.x);
+		best_size = get_best_size();
+
+		if(size.x >= best_size.x && size.y >= best_size.y) {
+			// Wrapping succeeded, we still fit vertically.
+			place(get_origin(), size);
+			return;
+		} else {
+			// Wrapping failed, we no longer fit.
+			// Reset the sizes of child widgets.
+			layout_initialise(true);
+		}
+	}
+
+	// Not enough space, ask the parent grid for more.
+	grid* parent = get_parent_grid();
+	if(parent != nullptr) {
+		parent->relayout();
+	} else {
+		get_window()->invalidate_layout();
+	}
 }
 
 point grid::recalculate_best_size()

src/gui/widgets/grid.hpp
@@ -221,6 +221,13 @@ public:
 	virtual void demand_reduce_height(const unsigned maximum_height) override;
 
 	/**
+	 * Attempts to lay out the grid without laying out the entire window.
+	 * If the grid needs to grow, asks the parent grid for more space, recursively.
+	 * If the grid is the top-level grid, falls back to laying out the whole window.
+	 */
+	void relayout();
+
+	/**
 	 * Recalculates the best size.
 	 *
 	 * This is used for scrollbar containers when they try to update their

src/gui/widgets/scrollbar_container.cpp
@@ -582,10 +582,11 @@ bool scrollbar_container::content_resize_request(const bool force_sizing)
 				  == widget::visibility::invisible)) {
 
 			DBG_GUI_L << LOG_HEADER
-					  << " can't use horizontal scrollbar, ask window.\n";
-			window* window = get_window();
-			assert(window);
-			window->invalidate_layout();
+					  << " can't use horizontal scrollbar, ask grid.\n";
+			layout_initialise(true);
+			grid* grid = get_parent_grid();
+			assert(grid);
+			grid->relayout();
 			return false;
 		}
 	}
@@ -598,10 +599,11 @@ bool scrollbar_container::content_resize_request(const bool force_sizing)
 				  == widget::visibility::invisible)) {
 
 			DBG_GUI_L << LOG_HEADER
-					  << " can't use vertical scrollbar, ask window.\n";
-			window* window = get_window();
-			assert(window);
-			window->invalidate_layout();
+					  << " can't use vertical scrollbar, ask grid.\n";
+			layout_initialise(true);
+			grid* grid = get_parent_grid();
+			assert(grid);
+			grid->relayout();
 			return false;
 		}
 	}

src/gui/widgets/widget.cpp
@@ -14,6 +14,7 @@
 
 #define GETTEXT_DOMAIN "wesnoth-lib"
 
+#include "gui/widgets/grid.hpp"
 #include "gui/widgets/settings.hpp"
 #include "gui/widgets/window.hpp"
 #include "gui/core/event/message.hpp"
@@ -135,6 +136,16 @@ const window* widget::get_window() const
 
 	// on error dynamic_cast returns nullptr which is what we want.
 	return dynamic_cast<const window*>(result);
+}
+
+grid* widget::get_parent_grid()
+{
+	widget* result = parent_;
+	while(result && dynamic_cast<grid*>(result) == nullptr) {
+		result = result->parent_;
+	}
+
+	return result ? dynamic_cast<grid*>(result) : nullptr;
 }
 
 dialogs::modal_dialog* widget::dialog()

src/gui/widgets/widget.hpp
@@ -32,6 +32,7 @@ namespace gui2
 struct builder_widget;
 namespace dialogs { class modal_dialog; }
 class window;
+class grid;
 
 namespace iteration
 {
@@ -176,9 +177,17 @@ public:
 	 * Get the parent window.
 	 *
 	 * @returns                   Pointer to parent window.
-	 * @retval nullptr               No parent window found.
+	 * @retval nullptr            No parent window found.
 	 */
 	window* get_window();
+
+	/**
+	 * Get the parent grid.
+	 *
+	 * @returns                 Pointer to parent grid.
+	 * @retval nullptr          No parent grid found.
+	 */
+	grid* get_parent_grid();
 
 	/** The constant version of @ref get_window. */
 	const window* get_window() const;

commit fca901389279e277b41bdb5751dab2f2dc4effd3

Fix a severe performance regression on filtering add-ons
For some reason, it only occurred in debug builds when I tested.

The problem was that partial relayout didn't respect
invalidate_layout_blocker. As a result, in situations such as populating a
listbox one entry at a time, the layout system ended up performing dozens
of full relayouts that, not surprisingly, is extremely expensive.

Regression from commit abc859612d12ae36142905f6c6eac081ab5ae155 [formerly e0eb41027d3acc08bdacbb727d2cf96f5fca7d5a] [formerly 31aff52bb1a545c7475c4978ce660e202143ff57 [formerly cf643e2bbfad22c46975f75415c1c2e9cd017929]] [formerly 46b89eee3118e3e3c2a4f59757ffd4ef25ce427b] [formerly 1b663fc184eb1f7a16dd8a5dad90acf6c38461f7] [formerly 03bb137d9c8219f50515619d3f42c3de11a05e0c].


Former-commit-id: e300d73c2c37c2d79d23bf19d4968917ed0f550b
Former-commit-id: 906342f1246a11edff6a3bdf575520960312c625
Former-commit-id: c588a63d186dd3d99f7f7b7d84f90157bd91ad35
Former-commit-id: 14aaa7194352543c50c4987aafa43ed61227a0cf [formerly 289b713043be05601c136347ab21187f0ead09e5]
Former-commit-id: 92bf722f72b196a84e2f8e45c23e0a2e1be8e1a0

src/gui/widgets/grid.cpp
@@ -385,6 +385,11 @@ void grid::demand_reduce_height(const unsigned /*maximum_height*/)
 
 void grid::request_placement(dispatcher&, const event::ui_event, bool& handled, bool&)
 {
+	if (get_window()->invalidate_layout_blocked()) {
+		handled = true;
+		return;
+	}
+
 	point size = get_size();
 	point best_size = calculate_best_size();
 	if(size.x >= best_size.x && size.y >= best_size.y) {

src/gui/widgets/window.hpp
@@ -256,6 +256,12 @@ public:
 		window& window_;
 	};
 
+	/** Is invalidate_layout blocked, see invalidate_layout_blocker. */
+	bool invalidate_layout_blocked() const
+	{
+		return invalidate_layout_blocked_;
+	}
+
 	/**
 	 * Updates the size of the window.
 	 *
@@ -521,7 +527,7 @@ private:
 	/** The variables of the canvas. */
 	wfl::map_formula_callable variables_;
 
-	/** Is invalidate layout blocked see invalidate_layout_blocker. */
+	/** Is invalidate_layout blocked, see invalidate_layout_blocker. */
 	bool invalidate_layout_blocked_;
 
 	/** Avoid drawing the window.  */