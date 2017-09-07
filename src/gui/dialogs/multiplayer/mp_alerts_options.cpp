fendrin/Wesnoth

commit 2a0d31fd07b9c40c20955da0e484aa8b9044c5ef

MP Alerts Options: simplify an overload disambiguation
Former-commit-id: 1c729580d25b96955b2cc5a0c9661c3ad2df64f7
Former-commit-id: 7e990cb533cd22c6a76538a9cfd22197cf822ccb
Former-commit-id: 35d554b29e26f16ca62a988769f2a45be6fa147b
Former-commit-id: d0f236f53d37225193347b16179d2c6f38ffce39 [formerly dbf8be5069b7c850fc60aa4c4cc84d951c531754]
Former-commit-id: 53ca81c3b9bd1feb733020b6aa172cf42b0f2927

src/gui/dialogs/multiplayer/mp_alerts_options.cpp
@@ -73,10 +73,7 @@ static toggle_button * setup_pref_toggle_button(const std::string & id, bool def
 		preferences::set(id, def);
 	}
 
-	//Needed to disambiguate overloaded function
-	void (*set) (const std::string &, bool) = &preferences::set;
-
-	connect_signal_mouse_left_click(*b, std::bind(set, id, std::bind(&toggle_button::get_value_bool, b)));
+	connect_signal_mouse_left_click(*b, std::bind([&, id]() { preferences::set(id, b->get_value_bool()); }));
 
 	return b;
 }

commit 56c50d819f718e44222efc4f080ff9768d045aaa

MP Alert Options: fix crash caused by 2a0d31fd07b9 [formerly 53ca81c3b9bd] [formerly d0f236f53d37 [formerly dbf8be5069b7]] [formerly 35d554b29e26] [formerly 7e990cb533cd] [formerly 1c729580d25b] (fixes bug #1945)
Accessing captured variable out of scope.


Former-commit-id: d1b0c661c4eccac99a156f2d05f86a2de56a57ed
Former-commit-id: 037a8a6275fd82a90574ca8152535e2ecd9129a0
Former-commit-id: 8116520b5a487343c81d47771adf8dcf77cae6b8
Former-commit-id: ffcf1809a0e79c8986752a4174df7abc0d2656f3 [formerly 656c1503554d441832591c49d0df70238aec74c9]
Former-commit-id: b599938379bbd3bac547cb5d9f7cd4391c513aa7

src/gui/dialogs/multiplayer/mp_alerts_options.cpp
@@ -73,7 +73,7 @@ static toggle_button * setup_pref_toggle_button(const std::string & id, bool def
 		preferences::set(id, def);
 	}
 
-	connect_signal_mouse_left_click(*b, std::bind([&, id]() { preferences::set(id, b->get_value_bool()); }));
+	connect_signal_mouse_left_click(*b, std::bind([b, id]() { preferences::set(id, b->get_value_bool()); }));
 
 	return b;
 }