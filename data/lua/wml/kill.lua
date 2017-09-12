fendrin/Wesnoth

commit 9ea559cb6e79e00513c19cafb7a844b9b5a20cb5

Fix [kill]ed units flashing briefly after death animation
Former-commit-id: 890aa28e17989a9e76244585f806adc25f2dbeee
Former-commit-id: 2f72b00f696abca604b860fef97466b2e915b98e
Former-commit-id: 546b9c328be159da175387b992716636775167e2
Former-commit-id: 61a7a0abf1a8e145833e367bfec9a240926899e1 [formerly 673631396c30ff12733beff7c74469b12b637e57]
Former-commit-id: 9628fa7e6e23aef69728260683113fd4f1d4cb43

data/lua/wml/kill.lua
@@ -70,7 +70,7 @@ function wesnoth.wml_actions.kill(cfg)
 			end
 			anim:run()
 		end
-		wesnoth.wml_actions.redraw{}
+		-- wesnoth.wml_actions.redraw{}
 
 		if can_fire then
 			wesnoth.fire_event("die", death_loc, killer_loc)
@@ -84,6 +84,8 @@ function wesnoth.wml_actions.kill(cfg)
 		end
 		-- Test that it's valid (and still on the map) first, in case the event erased (or extracted) it.
 		if unit.valid == "map" then unit:erase() end
+
+		wesnoth.wml_actions.redraw{}
 
 		number_killed = number_killed + 1
 	end

commit 9935554f86026a569d54286d1b7ce012cad067b1

Fix #1980: [modify_unit] makes the unit disappear for a frame or two
Regression from commit 9ea559cb6e79e00513c19cafb7a844b9b5a20cb5 [formerly 9628fa7e6e23aef69728260683113fd4f1d4cb43] [formerly 61a7a0abf1a8e145833e367bfec9a240926899e1 [formerly 673631396c30ff12733beff7c74469b12b637e57]] [formerly 546b9c328be159da175387b992716636775167e2] [formerly 2f72b00f696abca604b860fef97466b2e915b98e] [formerly 890aa28e17989a9e76244585f806adc25f2dbeee].

In that commit I removed a redraw after the death animation of a killed
unit has been played, but just to be safe, I then added a redraw after the
unit has been erased from the map. Turns out it's not a good idea because
[modify_unit] operates by killing and recreating the unit.

This commit simply removes the redraw after erasing. Hopefully we aren't
relying on [kill] triggering a redraw somewhere.

[ci skip]


Former-commit-id: 8fa44461edbf69008cd2337bef2c9bd1228326a7
Former-commit-id: 9827c9c0f47bb68843f31c2c94b98940a7564304
Former-commit-id: 7f00a511d95cb0dc4e18fe65edbc7bc9bb26f7f3
Former-commit-id: bb1f8cf6e20a7bf800a42fdd73ba8231b2b5ef75 [formerly 52c8b59e8b04fc4f5962f0477ede871ed1ae1695]
Former-commit-id: 9ecc2d2f2eaa3a85f1979e093341234dc368ebc9

data/lua/wml/kill.lua
@@ -85,7 +85,7 @@ function wesnoth.wml_actions.kill(cfg)
 		-- Test that it's valid (and still on the map) first, in case the event erased (or extracted) it.
 		if unit.valid == "map" then unit:erase() end
 
-		wesnoth.wml_actions.redraw{}
+		-- wesnoth.wml_actions.redraw{}
 
 		number_killed = number_killed + 1
 	end