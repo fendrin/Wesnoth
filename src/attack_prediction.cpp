fendrin/Wesnoth

commit 342c2c5e4ba9db28fcea90d0f5eca76f39d91c2f

Fix wrong calculation for chance to stay unscathed with a slowing unit
Only a minor difference in most cases, but enough to trigger the Monte
Carlo simulation assertion.

Fixes #1810.


Former-commit-id: 696e8480b36631ad5d5bbbdd93bc04a0c8926ce1
Former-commit-id: 93f7e7d3250995a18e543fe3b855c388e1dfa40c
Former-commit-id: a5e7425ac2612c0c819253e6b3d07250fa736f35
Former-commit-id: 4abf5e25f1d75182f26a4de3d8eccfe56ca15ade [formerly 9220654504f5d7adbb9b1d5a29a4749ffd9cab43]
Former-commit-id: b35b76f48d2a78bad4bd4ee21bb8fc629a30ee92

src/attack_prediction.cpp
@@ -280,6 +280,10 @@ public:
 
 	/// What is the chance that an indicated combatant (one of them) is at zero?
 	double prob_of_zero(bool check_a, bool check_b) const;
+	/// Sums the values in the specified row.
+	double row_sum(unsigned plane, unsigned row) const;
+	/// Sums the values in the specified column.
+	double col_sum(unsigned plane, unsigned column) const;
 	/// Sums the values in the specified plane.
 	void sum(unsigned plane, std::vector<double>& row_sums, std::vector<double>& col_sums) const;
 
@@ -937,6 +941,30 @@ double prob_matrix::prob_of_zero(bool check_a, bool check_b) const
 }
 
 /**
+ * Sums the values in the specified row.
+ */
+double prob_matrix::row_sum(unsigned plane, unsigned row) const
+{
+	double sum = 0.0;
+	for(unsigned col : used_cols_[plane]) {
+		sum += val(plane, row, col);
+	}
+	return sum;
+}
+
+/**
+ * Sums the values in the specified column.
+ */
+double prob_matrix::col_sum(unsigned plane, unsigned column) const
+{
+	double sum = 0.0;
+	for(unsigned row : used_rows_[plane]) {
+		sum += val(plane, row, column);
+	}
+	return sum;
+}
+
+/**
  * Sums the values in the specified plane.
  * The sum of each row is added to the corresponding entry in row_sums.
  * The sum of each column is added to the corresponding entry in col_sums.
@@ -1018,6 +1046,9 @@ public:
 
 	void forced_levelup_b();
 	void conditional_levelup_b();
+
+	using prob_matrix::row_sum;
+	using prob_matrix::col_sum;
 
 	// Its over, and here's the bill.
 	virtual void extract_results(
@@ -1816,6 +1847,15 @@ unsigned int fight_complexity(unsigned int num_slices,
 		   * ((opp_stats.slows || stats.is_slowed) ? 2 : 1) * stats.max_hp * opp_stats.max_hp;
 }
 
+/**
+ * Returns the plane index for the slow/no-slow state of the combatants.
+ */
+unsigned int plane_index(const battle_context_unit_stats& stats,
+	const battle_context_unit_stats& opp_stats)
+{
+	return (stats.is_slowed ? 1 : 0) | (opp_stats.is_slowed ? 2 : 0);
+}
+
 // Combat without chance of death, berserk, slow or drain is simple.
 void no_death_fight(const battle_context_unit_stats& stats,
 		const battle_context_unit_stats& opp_stats,
@@ -2057,6 +2097,19 @@ void complex_fight(attack_prediction_mode mode,
 			debug(("Combat ends:\n"));
 			pm->dump();
 		} while(--rounds && pm->dead_prob() < 0.99);
+
+		if(stats.slows) {
+			/* The calculation method for the "not hit" probability above is incorrect if either unit can slow.
+			 * Because a slowed unit deals less damage, it is more likely for the slowing unit to be alive if it
+			 * has hit the other unit. In that situation, the "not hit" probability can no longer be calculated
+			 * with a simple multiplication.
+			 * Instead, just fetch the probability from the combat matrix.
+			 */
+			opp_not_hit = pm->col_sum(plane_index(stats, opp_stats), opp_stats.hp);
+		}
+		if(opp_stats.slows) {
+			self_not_hit = pm->row_sum(plane_index(stats, opp_stats), stats.hp);
+		}
 	} else {
 		debug(("Using Monte Carlo simulation.\n"));
 

commit e3f2561e5ffdb8ff47f1d3e3a263c67a096fd8f1

Fix crash when the AI simulates battles with both swarm and slow
This happened if the AI simulated three fights for an unit that has swarm
and either unit was able to slow in the second fight.

Regression from commit 342c2c5e4ba9db28fcea90d0f5eca76f39d91c2f [formerly b35b76f48d2a78bad4bd4ee21bb8fc629a30ee92] [formerly 4abf5e25f1d75182f26a4de3d8eccfe56ca15ade [formerly 9220654504f5d7adbb9b1d5a29a4749ffd9cab43]] [formerly a5e7425ac2612c0c819253e6b3d07250fa736f35] [formerly 93f7e7d3250995a18e543fe3b855c388e1dfa40c] [formerly 696e8480b36631ad5d5bbbdd93bc04a0c8926ce1].

I didn't take into account that when swarm is used, the probability to stay
unscathed was supposed to be scaled by the probability of the whole combat
slice occurring in the first place. If either unit was able to slow,
complex_fight() calculated the unscaled probability instead, and when they
were added together, the combined probability went way above 100 %. That
triggered an assertion if the AI simulated one more fight for the affected
unit.


Former-commit-id: 877895531b42224a8a1edfc8244380f8b6f2edb0
Former-commit-id: 40daa8cd2349b8c48d28284329417ea0447a6b26
Former-commit-id: ecb5523e8122cb6ea6d016d32a17527142c3305f
Former-commit-id: 0471cb833aeea0dbf3c52601ea7bfaf4c8ccbf47 [formerly d3a8fba82282d858412c19a29ec84f02df21f7f9]
Former-commit-id: 278f660c242ee7d15bc30d32137f65b1bc46f350

src/attack_prediction.cpp
@@ -2066,6 +2066,8 @@ void complex_fight(attack_prediction_mode mode,
 		b_damage = b_slow_damage = stats.max_hp;
 	}
 
+	const double original_self_not_hit = self_not_hit;
+	const double original_opp_not_hit = opp_not_hit;
 	const double hit_chance = stats.chance_to_hit / 100.0;
 	const double opp_hit_chance = opp_stats.chance_to_hit / 100.0;
 
@@ -2107,10 +2109,10 @@ void complex_fight(attack_prediction_mode mode,
 			 * with a simple multiplication.
 			 * Instead, just fetch the probability from the combat matrix.
 			 */
-			opp_not_hit = pm->col_sum(plane_index(stats, opp_stats), opp_stats.hp);
+			opp_not_hit = original_opp_not_hit * pm->col_sum(plane_index(stats, opp_stats), opp_stats.hp);
 		}
 		if(opp_stats.slows) {
-			self_not_hit = pm->row_sum(plane_index(stats, opp_stats), stats.hp);
+			self_not_hit = original_self_not_hit * pm->row_sum(plane_index(stats, opp_stats), stats.hp);
 		}
 	} else {
 		debug(("Using Monte Carlo simulation.\n"));