fendrin/Wesnoth

commit 7637772f78e69611382aa8750fa474fa873a0464

GUI2/Window: minor cleanup
Simplified a setter and added some members to the ctor initializer list.


Former-commit-id: 0fd54c3ced72e7f01a76fd6abd02a6a7a4f81cc9
Former-commit-id: 568f0a2912598821e5c6844059b5f9f73ea261f3
Former-commit-id: 92e0ca9052c1994d034ff971f63578f07b6d18a9
Former-commit-id: a2d628fc4f35fd9a0199f226d614d673a55f7abe [formerly be70da0dc82d6e899315c447ea86eead41c271ce]
Former-commit-id: 895dfd5549782d512a7bb0447b27440166836c61

src/gui/widgets/window.hpp
@@ -463,9 +463,8 @@ public:
 		if(it != linked_size_.end()) {
 			return point(it->second.width, it->second.height);
 		}
-		else {
-			return point(-1, -1);
-		}
+
+		return point(-1, -1);
 	}
 
 	/**
@@ -482,13 +481,7 @@ public:
 
 	void set_exit_hook_ok_only(std::function<bool(window&)> func)
 	{
-		exit_hook_ = [func](window& w)->bool {
-			if(w.get_retval() == OK) {
-				return func(w);
-			}
-
-			return true;
-		};
+		exit_hook_ = [func](window& w)->bool { return (w.get_retval() == OK && func(w)) || true; };
 	}
 
 	/**
@@ -821,7 +814,7 @@ private:
 	void signal_handler_request_placement(const event::ui_event event,
 										  bool& handled);
 
-	std::function<bool(window&)> exit_hook_ = [](window&)->bool { return true; };
+	std::function<bool(window&)> exit_hook_;
 	std::function<void()> callback_next_draw_;
 };
 

src/gui/widgets/window.cpp
@@ -340,8 +340,9 @@ window::window(CVideo& video,
 #ifdef DEBUG_WINDOW_LAYOUT_GRAPHS
 	, debug_layout_(new debug_layout_graph(this))
 #endif
-	, event_distributor_(
-			  new event::distributor(*this, event::dispatcher::front_child))
+	, event_distributor_(new event::distributor(*this, event::dispatcher::front_child))
+	, exit_hook_([](window&)->bool { return true; })
+	, callback_next_draw_(nullptr)
 {
 	// We load the config in here as exception.
 	// Our caller did update the screen size so no need for us to do that again.

commit 620b8e4362c3c826189597ab1cc7ff1e47ebf47c

GUI2/Window: fixed behavior regression from 7637772f78 [formerly 895dfd5549] [formerly a2d628fc4f [formerly be70da0dc8]] [formerly 92e0ca9052] [formerly 568f0a2912] [formerly 0fd54c3ced]
The on-OK-only exit hook wrapper was always returning true.


Former-commit-id: 069093b166dfa2ad31d56919294b016a27201bc2
Former-commit-id: f236916dbedd855323260f2210ff3e482c9fe05c
Former-commit-id: dd2f27cd4ebec0c6f004b36b8257addd903d38b5
Former-commit-id: 45cb100aed761ce66bf59bb29ff74db5f46318d5 [formerly 3abd53521df97c5253f11ed9a4b1c944ef5d4ff0]
Former-commit-id: 5e1188a8fe1b239c463fbf3793f8a0ac9638eb95

src/gui/widgets/window.hpp
@@ -455,7 +455,7 @@ public:
 
 	void set_exit_hook_ok_only(std::function<bool(window&)> func)
 	{
-		exit_hook_ = [func](window& w)->bool { return (w.get_retval() == OK && func(w)) || true; };
+		exit_hook_ = [func](window& w)->bool { return w.get_retval() != OK || func(w); };
 	}
 
 	/**