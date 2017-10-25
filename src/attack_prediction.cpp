fendrin/Wesnoth

commit c7aa0516fdf8d31f805622c07ea2f174cdf481cf

New method to calculate chance to stay unscathed
@mattsc spotted that attack prediction sometimes gives incorrect results
for chance to stay unscathed when the other unit can die during the battle.

The old calculation algorithm worked by determining the chance that the
unit survives each unit unscathed, and multiplying the probabilities
together.

However, that method of probability calculations works only if the
attacks are independent, i.e. the probability of surviving *this* attack
unscathed doesn't depend on the chance of being already unscathed. And
that's not the case if either combatant can kill the other one.

If combatant A has killed combatant B, combatant B can't strike it any more
and therefore combatant A is more likely to *be* unscathed. Inversely, if A
is unscathed, it's more likely that B is dead. As a result, if A is
unscathed, A is also more likely to *stay* unscathed. The old method
couldn't take that into account and therefore produced incorrect results.

In my local tests, this new algorithm appears to produce correct results.
Only rounding errors (and drain ability) separate it from the HP
distribution tables.


Former-commit-id: a48111e6c6e4da9c10235b49ebd415ba07255b20
Former-commit-id: c843f4c904e27f4ed100a6a5a7bf4af4841ee5a0
Former-commit-id: 990c854ad9f8cddf73971f1fe3ae8713e2615e60
Former-commit-id: 961013818276600e2557457569b784c76d992a4b [formerly 02527e4a35f739933fd397e90e8af7eadcd2365a]
Former-commit-id: 5602880b6a335279e04dca59f83d69a243b55e61

src/attack_prediction.cpp
@@ -2070,6 +2070,10 @@ void complex_fight(attack_prediction_mode mode,
 	const double original_opp_not_hit = opp_not_hit;
 	const double hit_chance = stats.chance_to_hit / 100.0;
 	const double opp_hit_chance = opp_stats.chance_to_hit / 100.0;
+	double self_hit = 0.0;
+	double opp_hit = 0.0;
+	double self_hit_unknown = 1.0; // Probability we don't yet know if A will be hit or not
+	double opp_hit_unknown = 1.0; // Ditto for B
 
 	// Prepare the matrix that will do our calculations.
 	std::unique_ptr<combat_matrix> m;
@@ -2086,15 +2090,27 @@ void complex_fight(attack_prediction_mode mode,
 			for(unsigned int i = 0; i < max_attacks; ++i) {
 				if(i < strikes) {
 					debug(("A strikes\n"));
-					opp_not_hit *= 1.0 - hit_chance * (1.0 - pm->dead_prob_a());
+					double b_already_dead = pm->dead_prob_b();
 					pm->receive_blow_b(hit_chance);
 					pm->dump();
+
+					double first_hit = hit_chance * opp_hit_unknown;
+					opp_hit += first_hit;
+					opp_hit_unknown -= first_hit;
+					double this_hit_killed_b = (pm->dead_prob_b() - b_already_dead) / ((1.0 - b_already_dead) * (1.0 - pm->dead_prob_a()));
+					self_hit_unknown *= (1.0 - this_hit_killed_b);
 				}
 				if(i < opp_strikes) {
 					debug(("B strikes\n"));
-					self_not_hit *= 1.0 - opp_hit_chance * (1.0 - pm->dead_prob_b());
+					double a_already_dead = pm->dead_prob_a();
 					pm->receive_blow_a(opp_hit_chance);
 					pm->dump();
+
+					double first_hit = opp_hit_chance * self_hit_unknown;
+					self_hit += first_hit;
+					self_hit_unknown -= first_hit;
+					double this_hit_killed_a = (pm->dead_prob_a() - a_already_dead) / ((1.0 - a_already_dead) * (1.0 - pm->dead_prob_b()));
+					opp_hit_unknown *= (1.0 - this_hit_killed_a);
 				}
 			}
 
@@ -2102,11 +2118,14 @@ void complex_fight(attack_prediction_mode mode,
 			pm->dump();
 		} while(--rounds && pm->dead_prob() < 0.99);
 
+		self_not_hit = original_self_not_hit * (1.0 - self_hit);
+		opp_not_hit = original_opp_not_hit * (1.0 - opp_hit);
+
 		if(stats.slows) {
 			/* The calculation method for the "not hit" probability above is incorrect if either unit can slow.
 			 * Because a slowed unit deals less damage, it is more likely for the slowing unit to be alive if it
 			 * has hit the other unit. In that situation, the "not hit" probability can no longer be calculated
-			 * with a simple multiplication.
+			 * with simple addition.
 			 * Instead, just fetch the probability from the combat matrix.
 			 */
 			opp_not_hit = original_opp_not_hit * pm->col_sum(plane_index(stats, opp_stats), opp_stats.hp);

commit 2f0b854b80fe42329d4792ffcda3d7cc7d7e85ef

Fix #2117: crash if either combatant was guaranteed to die
Regression from commit c7aa0516fdf8d31f805622c07ea2f174cdf481cf [formerly 5602880b6a335279e04dca59f83d69a243b55e61] [formerly 961013818276600e2557457569b784c76d992a4b [formerly 02527e4a35f739933fd397e90e8af7eadcd2365a]] [formerly 990c854ad9f8cddf73971f1fe3ae8713e2615e60] [formerly c843f4c904e27f4ed100a6a5a7bf4af4841ee5a0] [formerly a48111e6c6e4da9c10235b49ebd415ba07255b20].

The code divided by zero and the probability to stay unscathed ended up as
NaN, which triggered an assertion failure if the AI simulated one more
fight for either combatant.


Former-commit-id: d0b153f9576b21ebd4929756786b5d8c7709ea7d
Former-commit-id: ccb70e287b5a75903db7ea9869451b8c38446eb5
Former-commit-id: 1e7ddfd4ca1bfb5ddd4829301ce98d17cc570376
Former-commit-id: b5aee109cb35bbd763c84dc895dd809147341757 [formerly b8685e98b8c0669a22ba762158b0cc4849db59f7]
Former-commit-id: 72e81818b2f775e8c6e3b3bd2790561c7df8568f

src/attack_prediction.cpp
@@ -2097,7 +2097,8 @@ void complex_fight(attack_prediction_mode mode,
 					double first_hit = hit_chance * opp_hit_unknown;
 					opp_hit += first_hit;
 					opp_hit_unknown -= first_hit;
-					double this_hit_killed_b = (pm->dead_prob_b() - b_already_dead) / ((1.0 - b_already_dead) * (1.0 - pm->dead_prob_a()));
+					double both_were_alive = (1.0 - b_already_dead) * (1.0 - pm->dead_prob_a());
+					double this_hit_killed_b = both_were_alive != 0.0 ? (pm->dead_prob_b() - b_already_dead) / both_were_alive : 1.0;
 					self_hit_unknown *= (1.0 - this_hit_killed_b);
 				}
 				if(i < opp_strikes) {
@@ -2109,7 +2110,8 @@ void complex_fight(attack_prediction_mode mode,
 					double first_hit = opp_hit_chance * self_hit_unknown;
 					self_hit += first_hit;
 					self_hit_unknown -= first_hit;
-					double this_hit_killed_a = (pm->dead_prob_a() - a_already_dead) / ((1.0 - a_already_dead) * (1.0 - pm->dead_prob_b()));
+					double both_were_alive = (1.0 - a_already_dead) * (1.0 - pm->dead_prob_b());
+					double this_hit_killed_a = both_were_alive != 0.0 ? (pm->dead_prob_a() - a_already_dead) / both_were_alive : 1.0;
 					opp_hit_unknown *= (1.0 - this_hit_killed_a);
 				}
 			}