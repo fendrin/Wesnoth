fendrin/Wesnoth

commit a243f915d5852ca443f4b3c0442dc60047a18cd7

Rewrote the code that restarts the process
Unicode command-line parameters now work on Windows.


Former-commit-id: 3faf506b62b8aafd5823551fef08537ef0082dc2
Former-commit-id: ad09cbb707a8bfc7e6a43d653a9947e4a99b7c79
Former-commit-id: 55f25ec0aba4572ab36e533cd1c851617d49b7fd
Former-commit-id: 34449d46a958b4ecdee1589da8eec49a61c6add4 [formerly 530e0b05bdf123b15716a05405e98f0049398e51]
Former-commit-id: 2bffd119f3f88d908aa3cef9422afbfa688b9281

src/wesnoth.cpp
@@ -926,6 +926,40 @@ static void wesnoth_terminate_handler(int) {
 }
 #endif
 
+#ifdef _WIN32
+static void restart_process(const std::vector<std::string>& commandline)
+{
+	wchar_t process_path[MAX_PATH];
+	SetLastError(ERROR_SUCCESS);
+	GetModuleFileNameW(NULL, process_path, MAX_PATH);
+	if (GetLastError() != ERROR_SUCCESS)
+	{
+		throw std::exception("Failed to retrieve the process path");
+	}
+
+	std::wstring commandline_str = unicode_cast<std::wstring>(utils::join(commandline, " "));
+	// CreateProcessW is allowed to modify the passed command line.
+	// Therefore we need to copy it.
+	wchar_t* commandline_c_str = new wchar_t[commandline_str.length() + 1];
+	commandline_str.copy(commandline_c_str, commandline_str.length());
+	commandline_c_str[commandline_str.length()] = L'\0';
+
+	STARTUPINFOW startup_info;
+	ZeroMemory(&startup_info, sizeof(startup_info));
+	startup_info.cb = sizeof(startup_info);
+	PROCESS_INFORMATION process_info;
+	ZeroMemory(&process_info, sizeof(process_info));
+
+	CreateProcessW(process_path, commandline_c_str, NULL, NULL,
+		false, ABOVE_NORMAL_PRIORITY_CLASS, NULL, NULL, &startup_info, &process_info);
+
+	CloseHandle(process_info.hProcess);
+	CloseHandle(process_info.hThread);
+
+	std::exit(EXIT_SUCCESS);
+}
+#endif
+
 #if defined(__native_client__) || (defined(__APPLE__) && SDL_VERSION_ATLEAST(2, 0, 0))
 extern "C" int wesnoth_main(int argc, char** argv);
 int wesnoth_main(int argc, char** argv)
@@ -988,12 +1022,7 @@ int main(int argc, char** argv)
 	if (!getenv("OMP_WAIT_POLICY")) {
 		_putenv_s("OMP_WAIT_POLICY", "PASSIVE");
 		args[0] = utils::quote(args[0]);
-		// NewArgs contains one more element than args in order to be
-		// NULL terminated (its buffer will be passed to _execv() as argv).
-		std::vector<const char*> newArgs(args.size() + 1, NULL);
-		std::transform(args.begin(), args.end(),
-			newArgs.begin(), boost::bind(&std::string::c_str, _1));
-		_execv(argv[0], &newArgs[0]);
+		restart_process(args);
 	}
 #endif
 #endif //_OPENMP

commit fad2e41b84e143378e3a06636adee590c7a3f4ad

Fix command line arguments with spaces being broken on Windows
This was a regression from a243f915d5852 [formerly 2bffd119f3f88] [formerly 34449d46a958b [formerly 530e0b05bdf12]] [formerly 55f25ec0aba45] [formerly ad09cbb707a8b] [formerly 3faf506b62b8a]. Since quotes got stripped from cl arguments when they were parsed,
and since the utils::join call expected a space as a delimiter, the arguments would get put together incorrectly.

One possible fix would have been to wrap every argument in quotes, but that could potentially some issues for
certain non-quoted arguments. Instead, we just take the original command line output from the console.


Former-commit-id: edf134cc168d60445573fcd14b26e694d94deac8
Former-commit-id: 89abbfadb56be38706344c65e7f35ad96aec7078
Former-commit-id: 096296eb6bf771cb215adb840c7bef40c8b5045b
Former-commit-id: 6d3fc9ee7b6fcd741b8f2abec602cd707f25862e [formerly 83f830012c86116d5303940a04e3b2860a88c4a8]
Former-commit-id: 9c632b130094452f7bb8057bb4213a81a37d9830

src/wesnoth.cpp
@@ -895,7 +895,7 @@ static void wesnoth_terminate_handler(int) {
 #endif
 
 #if defined(_OPENMP) && _MSC_VER >= 1600
-static void restart_process(const std::vector<std::string>& commandline)
+static void restart_process()
 {
 	wchar_t process_path[MAX_PATH];
 	SetLastError(ERROR_SUCCESS);
@@ -905,7 +905,7 @@ static void restart_process(const std::vector<std::string>& commandline)
 		throw std::runtime_error("Failed to retrieve the process path");
 	}
 
-	std::wstring commandline_str = unicode_cast<std::wstring>(utils::join(commandline, " "));
+	std::wstring commandline_str(GetCommandLineW());
 	// CreateProcessW is allowed to modify the passed command line.
 	// Therefore we need to copy it.
 	wchar_t* commandline_c_str = new wchar_t[commandline_str.length() + 1];
@@ -997,8 +997,7 @@ int main(int argc, char** argv)
 #elif _MSC_VER >= 1600
 	if (!getenv("OMP_WAIT_POLICY")) {
 		_putenv_s("OMP_WAIT_POLICY", "PASSIVE");
-		args[0] = utils::quote(args[0]);
-		restart_process(args);
+		restart_process();
 	}
 #endif
 #endif //_OPENMP